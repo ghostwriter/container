350dba9e5a4087dee6fa3c02f2e20735978e3ce9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-01 17:00:34 -0400

    Add test fixtures
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/Dummy.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\Tests\Fixture;
+
+class Dummy implements DummyInterface
+{
+    public function __construct()
+    {
+    }
+}
---
+++ tests/Fixture/ServiceProvider/FoobarWithDependencyServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\Tests\Fixture\ServiceProvider;
+
+use Ghostwriter\Container\Contract\ContainerInterface;
+use Ghostwriter\Container\Contract\ServiceProviderInterface;
+use Ghostwriter\Container\Tests\Fixture\Bar;
+use Ghostwriter\Container\Tests\Fixture\Baz;
+use Ghostwriter\Container\Tests\Fixture\Dummy;
+use Ghostwriter\Container\Tests\Fixture\Extension\FoobarExtension;
+use Ghostwriter\Container\Tests\Fixture\Foo;
+use stdClass;
+use Throwable;
+
+class FoobarWithDependencyServiceProvider implements ServiceProviderInterface
+{
+    private Dummy $dummy;
+    public function __construct(Dummy $dummy)
+    {
+        $this->dummy = $dummy;
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function __invoke(ContainerInterface $container): void
+    {
+        $container->set('dummy', $this->dummy);
+        $container->bind('foobar', stdClass::class);
+        $container->bind(Foo::class);
+        $container->bind(Bar::class);
+        $container->bind(Baz::class);
+        $container->add('foobar', $container->get(FoobarExtension::class));
+    }
+}
