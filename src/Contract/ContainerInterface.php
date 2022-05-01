95c6a8bfc43c90a34b674e380e8333a0c4cbc58d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-01 16:59:43 -0400

    Update ContainerInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/ContainerInterface.php
+++ src/Contract/ContainerInterface.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Ghostwriter\Container\Contract;
 
 use ArrayAccess;
+use Ghostwriter\Container\Container;
 use Ghostwriter\Container\Contract\Exception\NotFoundExceptionInterface;
+use Ghostwriter\Container\Exception\CircularDependencyException;
 use Ghostwriter\Container\Exception\InvalidArgumentException;
+use Ghostwriter\Container\Exception\LogicException;
+use Ghostwriter\Container\Exception\NotFoundException;
 use Ghostwriter\Container\Exception\NotInstantiableException;
 use Psr\Container\ContainerExceptionInterface as PsrContainerExceptionInterface;
 use Psr\Container\ContainerInterface as PsrContainerInterface;
@@ -14,10 +18,29 @@ use Psr\Container\NotFoundExceptionInterface as PsrNotFoundExceptionInterface;
 
 /**
  * An extendable, closure based dependency injection container.
+ *
+ * @template mixed
  */
 interface ContainerInterface extends ArrayAccess, PsrContainerInterface
 {
     public const ALIASES = 'aliases';
+
+    public const DEFAULT_SERVICES = [
+        self::ALIASES      => [
+            self::class    => Container::class,
+            PsrContainerInterface::class => Container::class,
+        ],
+        self::DEPENDENCIES => [],
+        self::EXTENSIONS   => [
+            Container::class => [],
+        ],
+        self::FACTORIES    => [],
+        self::PROVIDERS    => [],
+        self::SERVICES     => [
+            Container::class => null,
+        ],
+        self::TAGS         => [],
+    ];
 
     public const DEPENDENCIES = 'dependencies';
 
@@ -31,78 +54,122 @@ interface ContainerInterface extends ArrayAccess, PsrContainerInterface
 
     public const TAGS = 'tags';
 
+    /** Destroy the "static" instance of this container. */
+    public function __destruct();
+
     /**
-     * Provides an alternative name for a registered service.
+     * @throws BadMethodCallException If "__clone()" method is called. */
+    public function __clone();
+
+    /** @throws BadMethodCallException If "__serialize()" method is called. */
+    public function __serialize(): array;
+
+    /**
+     * @throws BadMethodCallException If "__unserialize()" method is called. */
+    public function __unserialize(array $data): void;
+
+    /**
+     * Add a service extension.
      *
-     * Should map one alias to a service id, or another alias (aliases are recursively resolved)
+     * @throws LogicException If $extension is already registered.
+     */
+    public function add(string $id, ExtensionInterface $extension): void;
+
+    /**
+     * Provide an alternative name for a registered service.
      *
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * @throws LogicException If $alias and $id are the same.
+     * @throws InvalidArgumentException If $alias or $id is empty.
+     * @throws NotFoundException If $id has not been registered.
      */
     public function alias(string $alias, string $id): void;
 
     /**
-     * Binds abstract classes and interfaces to concrete implementations
+     * Bind abstract classes or interfaces to concrete implementations
      *
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * @throws InvalidArgumentException If $abstract is empty.
+     * @throws LogicException If $abstract is already registered.
      */
     public function bind(string $abstract, ?string $concrete = null): void;
 
     /**
-     * Given a fully qualified class name, it will return a new instance of that class.
+     * Create an object using the given Container to resolve dependencies.
      *
-     * @param class-string $class
-     * @param array<string,mixed> $arguments Optional constructor arguments to use build the new class instance.
-     * @throws InvalidArgumentException The argument keys MUST be of type non-empty string.
-     * @throws NotInstantiableException If the class name is not instantiable; (is an interface or an abstract class).
-     * @throws PsrNotFoundExceptionInterface No entry or class found for the given name.
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * @param class-string|string $class The class name.
+     * @param array<string,mixed> $arguments Optional constructor arguments passed to build the new class instance.
+     * @throws CircularDependencyException If a circular dependency is detected.
+     * @throws NotInstantiableException If $class is not instantiable; (is an interface or an abstract class).
      */
     public function build(string $class, array $arguments = []): object;
 
     /**
      * "Extend" a service object in the container.
      *
-     * Callback runs after creating an object, using a registered factory with the given Service ID.
-     *
-     * @param callable(ContainerInterface,object):object $extension
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * @param callable(self,object):void $extension
+     * @throws InvalidArgumentException If $class is empty.
      */
     public function extend(string $class, callable $extension): void;
 
     /**
-     * @throws InvalidArgumentException Invalid identifier.
-     * @throws PsrNotFoundExceptionInterface No entry was found for **this** identifier.
-     * @throws PsrContainerExceptionInterface Error while retrieving the entry.
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * Instantiate and return the service with the given id.
+     *
+     * Note: This method will return the same instance on subsequent calls.
+     *
+     * @throws InvalidArgumentException If $id is empty.
+     * @throws NotFoundException If $id is not registered.
+     * @throws CircularDependencyException If a circular dependency is detected.
+     * @throws NotInstantiableException If $class is not instantiable; (is an interface or an abstract class).
+     * @throws PsrNotFoundExceptionInterface|NotFoundExceptionInterface No entry was found for **this** identifier.
+     * @throws PsrContainerExceptionInterface|ContainerExceptionInterface Error while retrieving the entry.
      */
     public function get(string $id): mixed;
 
+    /** Instantiate and return the Container */
+    public static function getInstance(): self;
+
+    /** Determine if a service $id exists in the Container */
     public function has(string $id): bool;
 
+    /** @param string $offset */
+    public function offsetExists(mixed $offset): bool;
+
+    /** @param string $offset */
+    public function offsetGet(mixed $offset): mixed;
+
+    /** @param string $offset */
+    public function offsetSet(mixed $offset, mixed $value): void;
+
+    /** @param string $offset */
+    public function offsetUnset(mixed $offset): void;
+
     /**
-     * @throws ContainerExceptionInterface
+     * Register a service provider.
+     *
+     * @throws LogicException If $serviceProvider is already registered.
      */
     public function register(ServiceProviderInterface $serviceProvider): void;
 
     /**
      * Remove a registered service.
      *
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * @throws InvalidArgumentException If the service $id is empty.
+     * @throws NotFoundException If the service $id is not registered.
      */
     public function remove(string $id): void;
+
+    /**
+     * Resolves an alias to the service id.
+     *
+     * @throws InvalidArgumentException If the service $id is empty.
+     */
+    public function resolve(string $id): string;
 
     /**
      * Assigns a service on the given container.
      *
      * @param iterable<string> $tags
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * @throws InvalidArgumentException If the service $id is empty.
+     * @throws LogicException If the service $id is already registered.
      */
     public function set(string $id, mixed $value, iterable $tags = []): void;
 
@@ -110,8 +177,7 @@ interface ContainerInterface extends ArrayAccess, PsrContainerInterface
      * Assign a set of tags to a given service id.
      *
      * @param iterable<string> $tags
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * @throws InvalidArgumentException If the service $id or a service tag in $tags is empty.
      */
     public function tag(string $id, iterable $tags): void;
 
