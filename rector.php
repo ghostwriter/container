8e223f3d2e2e593d94ff2aa5a86ae1a5b1afc408
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-01 17:09:15 -0400

    Create rector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ rector.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector;
+use Rector\Set\ValueObject\SetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\CodingStyle\Enum\PreferenceSelfThis;
+use Rector\Renaming\ValueObject\MethodCallRename;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
+use Rector\CodingStyle\Rector\MethodCall\PreferThisOrSelfMethodCallRector;
+use Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector;
+use Rector\Renaming\Rector\FileWithoutNamespace\PseudoNamespaceToNamespaceRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    // $rectorConfig->importNames();
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_80,
+        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        PHPUnitSetList::PHPUNIT_EXCEPTION,
+        PHPUnitSetList::REMOVE_MOCKS,
+        PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
+        PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::EARLY_RETURN,
+        SetList::NAMING,
+        SetList::NAMING,
+        SetList::PRIVATIZATION,
+        SetList::PSR_4,
+        SetList::RECTOR_CONFIG,
+        SetList::TYPE_DECLARATION_STRICT,
+        SetList::TYPE_DECLARATION,
+    ]);
+    $rectorConfig->paths([
+        __DIR__ . '/rector.php',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+    $rectorConfig->phpVersion(PhpVersion::PHP_80);
+
+    // prefer self:: over $this for phpunit
+    $rectorConfig->ruleWithConfiguration(
+        PreferThisOrSelfMethodCallRector::class,
+        [TestCase::class => PreferenceSelfThis::PREFER_SELF()]
+    );
+
+    // register single rule
+    $rectorConfig->rule(TypedPropertyRector::class);
+    $rectorConfig->ruleWithConfiguration(
+        RenameMethodRector::class,
+        [
+            new MethodCallRename(TestCase::class, 'setExpectedException', 'expectedException'),
+            new MethodCallRename(TestCase::class, 'setExpectedExceptionRegExp', 'expectedException')
+        ]
+    );
+
+    // $rectorConfig->ruleWithConfiguration(RenameNamespaceRector::class, [
+    //     'Old\Name' => 'New\name',
+    // ]);
+
+    $rectorConfig->skip([
+        __DIR__ . '*/tests/Fixture/*',
+        __DIR__ . '*/vendor/*',
+        /** CallableThisArrayToAnonymousFunctionRector::class, */
+        RepeatedLiteralToClassConstantRector::class,
+        PseudoNamespaceToNamespaceRector::class,
+        StringClassNameToClassConstantRector::class,
+        AddDoesNotPerformAssertionToNonAssertingTestRector::class,
+    ]);
+};
