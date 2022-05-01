4c2d025975fbeed7571536d1f98e6c0fa6becb1f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-01 17:01:14 -0400

    Update LogicException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/LogicException.php
+++ src/Exception/LogicException.php
@@ -11,21 +11,6 @@ use function sprintf;
 
 final class LogicException extends PHPLogicException implements ContainerExceptionInterface
 {
-    public static function dontClone(string $class): self
-    {
-        return new self(sprintf('Please do not clone "%s" instances.', $class));
-    }
-
-    public static function dontSerialize(string $class): self
-    {
-        return new self(sprintf('Please do not serialize "%s" instances.', $class));
-    }
-
-    public static function dontUnserialize(string $class): self
-    {
-        return new self(sprintf('Please do not unserialize "%s" instances.', $class));
-    }
-
     public static function serviceAlreadyRegistered(string $id): self
     {
         return new self(sprintf('Service "%s" has already been registered.', $id));
@@ -36,18 +21,13 @@ final class LogicException extends PHPLogicException implements ContainerExcepti
         return new self(sprintf('Service "%s" cannot alias itself.', $id));
     }
 
-    public static function serviceProviderAlreadyRegistered(string $id): self
-    {
-        return new self(sprintf('Service provider "%s" has already been registered.', $id));
-    }
-
     public static function serviceExtensionAlreadyRegistered(string $id): self
     {
         return new self(sprintf('Service extension "%s" has already been registered.', $id));
     }
 
-    public static function serviceTagAlreadyRegistered(string $tag, string $id): self
+    public static function serviceProviderAlreadyRegistered(string $id): self
     {
-        return new self(sprintf('Service tag "%s" has already been registered for service "%s".', $tag, $id));
+        return new self(sprintf('Service provider "%s" has already been registered.', $id));
     }
 }
