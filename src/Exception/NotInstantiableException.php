88eaa32def5a71ab268c045c01387a29b8d0a3c9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-09-14 10:47:10 -0500

    Update NotInstantiableException.php

--- src/Exception/NotInstantiableException.php
+++ src/Exception/NotInstantiableException.php
@@ -10,19 +10,28 @@ use function sprintf;
 
 final class NotInstantiableException extends PHPRuntimeException implements ContainerExceptionInterface
 {
+    /**
+     * @param class-string $id
+     */
     public static function abstractClassOrInterface(string $id): self
     {
         return new self(sprintf('Class "%s" is an abstract class or interface; not instantiable.', $id));
     }
 
+    /**
+     * @param class-string $class
+     */
     public static function classDoseNotExist(string $class): self
     {
         return new self(sprintf('Class "%s" dose not exist.', $class));
     }
 
+    /**
+     * @param class-string $class
+     */
     public static function unresolvableParameter(string $parameter, string $class, string $method): self
     {
-        $isFunction = $class === '';
+        $isFunction = '' === $class;
 
         return new self(sprintf(
             'Unresolvable %s parameter "$%s" in "%s%s"; does not have a default value.',
