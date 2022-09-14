17a530928728e65064e1fb5d1e9b72e1a2827566
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-09-14 10:49:57 -0500

    Update CircularDependencyException.php

--- src/Exception/CircularDependencyException.php
+++ src/Exception/CircularDependencyException.php
@@ -6,13 +6,7 @@ namespace Ghostwriter\Container\Exception;
 
 use Ghostwriter\Container\Contract\ContainerExceptionInterface;
 use RuntimeException as PHPRuntimeException;
-use function implode;
-use function sprintf;
 
 final class CircularDependencyException extends PHPRuntimeException implements ContainerExceptionInterface
 {
-    public static function detected(string $class, array $stack): self
-    {
-        return new self(sprintf('Circular dependency: %s -> %s', implode(' -> ', $stack), $class));
-    }
 }
