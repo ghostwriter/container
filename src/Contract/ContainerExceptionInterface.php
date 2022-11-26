d94ecb60125636361cc81ef07c92ad59daedd705
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-11-26 11:56:57 -0600

    Update ContainerExceptionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/ContainerExceptionInterface.php
+++ src/Contract/ContainerExceptionInterface.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container\Contract;
 
-use Psr\Container\ContainerExceptionInterface as PsrContainerExceptionInterface;
 use Throwable;
 
-interface ContainerExceptionInterface extends PsrContainerExceptionInterface, Throwable
+interface ContainerExceptionInterface extends Throwable
 {
 }
