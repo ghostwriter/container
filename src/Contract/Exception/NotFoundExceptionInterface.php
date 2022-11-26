27df02e786b3e228217ce640fc414eb15d42b5ea
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-11-26 11:54:45 -0600

    Update NotFoundExceptionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/Exception/NotFoundExceptionInterface.php
+++ src/Contract/Exception/NotFoundExceptionInterface.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace Ghostwriter\Container\Contract\Exception;
 
 use Ghostwriter\Container\Contract\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface as PsrNotFoundExceptionInterface;
 
-interface NotFoundExceptionInterface extends ContainerExceptionInterface, PsrNotFoundExceptionInterface
+interface NotFoundExceptionInterface extends ContainerExceptionInterface
 {
 }
