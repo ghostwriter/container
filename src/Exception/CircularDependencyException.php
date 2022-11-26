74bf5a194faf5bb97d0ef486c216d3948881c8b1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-11-26 11:54:33 -0600

    Update CircularDependencyException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/CircularDependencyException.php
+++ src/Exception/CircularDependencyException.php
@@ -6,7 +6,16 @@ namespace Ghostwriter\Container\Exception;
 
 use Ghostwriter\Container\Contract\ContainerExceptionInterface;
 use RuntimeException as PHPRuntimeException;
+use Throwable;
 
 final class CircularDependencyException extends PHPRuntimeException implements ContainerExceptionInterface
 {
+    public function __construct(string $class, array $dependencies, int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct(sprintf(
+            'Circular dependency: %s -> %s',
+            implode(' -> ', $dependencies),
+            $class
+        ), $code, $previous);
+    }
 }
