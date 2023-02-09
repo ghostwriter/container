ccaadd2fea7d307577f42ca206ee17041a29a3d0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-02-08 19:23:48 -0600

    Update ecs.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- ecs.php
+++ ecs.php
@@ -2,14 +2,33 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\Casing\ConstantCaseFixer;
+use PhpCsFixer\Fixer\ClassNotation\FinalClassFixer;
+use PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer;
+use PhpCsFixer\Fixer\ClassNotation\OrderedInterfacesFixer;
+use PhpCsFixer\Fixer\ClassNotation\OrderedTraitsFixer;
+use PhpCsFixer\Fixer\ClassNotation\SelfStaticAccessorFixer;
 
+use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
+use PhpCsFixer\Fixer\FunctionNotation\StaticLambdaFixer;
+use PhpCsFixer\Fixer\Import\GlobalNamespaceImportFixer;
 use PhpCsFixer\Fixer\Import\GroupImportFixer;
+use PhpCsFixer\Fixer\Import\NoLeadingImportSlashFixer;
+use PhpCsFixer\Fixer\Import\NoUnneededImportAliasFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+use PhpCsFixer\Fixer\Import\SingleImportPerStatementFixer;
+use PhpCsFixer\Fixer\Import\SingleLineAfterImportsFixer;
 use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
 use PhpCsFixer\Fixer\Phpdoc\GeneralPhpdocAnnotationRemoveFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocAlignFixer;
 use PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer;
 use PhpCsFixer\Fixer\Phpdoc\PhpdocTrimFixer;
+use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestCaseStaticMethodCallsFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->import(__DIR__ . '/vendor/ghostwriter/coding-standard/ecs.php');
@@ -27,8 +46,60 @@ return static function (ECSConfig $ecsConfig): void {
         GroupImportFixer::class,
         BinaryOperatorSpacesFixer::class,
         GeneralPhpdocAnnotationRemoveFixer::class,
-        PhpdocLineSpanFixer::class,
-        PhpdocTrimFixer::class,
+        //        PhpdocLineSpanFixer::class,
+        //        PhpdocTrimFixer::class,
         YodaStyleFixer::class,
+    ]);
+
+    $ecsConfig->rules([
+        SelfStaticAccessorFixer::class,
+        OrderedTraitsFixer::class,
+        FinalClassFixer::class,
+        StaticLambdaFixer::class,
+        NoLeadingImportSlashFixer::class,
+        NoUnneededImportAliasFixer::class,
+        NoUnusedImportsFixer::class,
+        SingleImportPerStatementFixer::class,
+        SingleLineAfterImportsFixer::class,
+    ]);
+
+    // this way you can add sets - group of rules
+    $ecsConfig->sets([
+        // run and fix, one by one
+        SetList::PHPUNIT,
+        SetList::SPACES,
+        SetList::ARRAY,
+        SetList::DOCBLOCK,
+        SetList::NAMESPACES,
+        SetList::COMMENTS,
+        SetList::PSR_12,
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(GlobalNamespaceImportFixer::class, [
+        'import_classes' => true,
+        'import_constants' => true,
+        'import_functions' => true,
+    ]);
+    $ecsConfig->ruleWithConfiguration(OrderedImportsFixer::class, [
+        'imports_order' => ['class', 'const', 'function'],
+    ]);
+    $ecsConfig->ruleWithConfiguration(PhpdocAlignFixer::class, [
+        'tags' => ['method', 'param', 'property', 'return', 'throws', 'type', 'var'],
+        'align'=>'left',
+    ]);
+    $ecsConfig->ruleWithConfiguration(PhpUnitTestCaseStaticMethodCallsFixer::class, [
+        'call_type' => 'self',
+    ]);
+    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
+        'syntax' => 'short',
+    ]);
+    $ecsConfig->ruleWithConfiguration(ConstantCaseFixer::class, [
+        'case' => 'lower',
+    ]);
+    $ecsConfig->ruleWithConfiguration(OrderedClassElementsFixer::class, [
+        'sort_algorithm' => 'alpha',
+    ]);
+    $ecsConfig->ruleWithConfiguration(OrderedInterfacesFixer::class, [
+        'order' => 'alpha',
     ]);
 };
