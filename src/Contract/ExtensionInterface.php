6e42f30d1cccbb8d420a47f141c377deb2d5d079
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-02-08 19:20:14 -0600

    Update ExtensionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/ExtensionInterface.php
+++ src/Contract/ExtensionInterface.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container\Contract;
 
+/**
+ * An extension is an invokable object that extends an object; created using the given container instance.
+ *
+ * @template TObject of object
+ */
 interface ExtensionInterface
 {
     /**
-     * Extends a service on the given container.
+     * Extend a service on the given container.
+     *
+     * @return TObject
      */
     public function __invoke(ContainerInterface $container, object $service): object;
 }
