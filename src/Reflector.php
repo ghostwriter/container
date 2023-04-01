9bde3a00b6a2950e1e06f7a3798d26c5edaf32a9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-01 14:49:50 -0500

    Create Reflector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Reflector.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container;
+
+use Ghostwriter\Container\Contract\ContainerExceptionInterface;
+use Ghostwriter\Container\Contract\ContainerInterface;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionParameter;
+use RuntimeException;
+
+final class Reflector
+{
+    /**
+     * @template TObject of object
+     * @var array<class-string<TObject>,ReflectionClass<TObject>>
+     */
+    private array $cache = [];
+    public function reflect(string $class): ReflectionClass
+    {
+        try {
+            $reflectionClass = $this->cache[$class] ??= new ReflectionClass($class);
+
+            if (! $reflectionClass->isInstantiable()) {
+                throw new ReflectorException(sprintf('Class "%s" is not instantiable.', $class));
+            }
+        } catch (ReflectionException) {
+            throw new ReflectorException(sprintf('Class "%s" does not exist.', $class));
+        }
+
+        return $reflectionClass;
+    }
+}
