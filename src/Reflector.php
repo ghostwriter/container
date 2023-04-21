fccabe1a209bdb32435d103c08198fdf36ce0791
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-21 07:43:48 -0500

    Update Reflector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Reflector.php
+++ src/Reflector.php
@@ -6,6 +6,7 @@ namespace Ghostwriter\Container;
 
 use Ghostwriter\Container\Tests\Unit\ReflectorTest;
 use ReflectionClass;
+use ReflectionFunction;
 use Throwable;
 
 /** @see ReflectorTest */
@@ -16,12 +17,28 @@ final class Reflector
      *
      * @param class-string<TClass> $class
      *
+     * @throws ReflectorException
+     *
      * @return ReflectionClass<TClass>
      */
     public static function getReflectionClass(string $class): ReflectionClass
     {
         try {
             return new ReflectionClass($class);
+        } catch (Throwable $throwable) {
+            throw new ReflectorException($throwable->getMessage());
+        }
+    }
+
+    /**
+     * @param callable|callable-string $function
+     *
+     * @throws ReflectorException
+     */
+    public static function getReflectionFunction(callable|string $function): ReflectionFunction
+    {
+        try {
+            return new ReflectionFunction($function);
         } catch (Throwable $throwable) {
             throw new ReflectorException($throwable->getMessage());
         }
