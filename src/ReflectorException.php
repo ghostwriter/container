dcaeb01a3cd0c6a55183b25676ca30210a0e4f4d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-26 07:52:33 -0500

    Update ReflectorException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ReflectorException.php
+++ src/ReflectorException.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container;
 
-use Ghostwriter\Container\Contract\ContainerExceptionInterface;
 use RuntimeException;
 
-final class ReflectorException extends RuntimeException implements ContainerExceptionInterface
+final class ReflectorException extends RuntimeException implements ExceptionInterface
 {
 }
