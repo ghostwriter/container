02827e179c9e42769a22a77e0161eaf347b309e8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 14:13:19 -0600

    Create NotFoundExceptionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Exception/NotFoundExceptionInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\Interface\Exception;
+
+use Ghostwriter\Container\Interface\ContainerExceptionInterface;
+
+interface NotFoundExceptionInterface extends ContainerExceptionInterface
+{
+}
