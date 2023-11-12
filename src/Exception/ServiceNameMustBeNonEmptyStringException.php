5351e3a06d8147423bc4764e8a7e4a241d33d382
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 16:00:25 -0600

    Update ServiceNameMustBeNonEmptyStringException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ServiceNameMustBeNonEmptyStringException.php
+++ src/Exception/ServiceNameMustBeNonEmptyStringException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container\Exception;
 
-use Ghostwriter\Container\Interface\ContainerExceptionInterface;
+use Ghostwriter\Container\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class ServiceNameMustBeNonEmptyStringException extends InvalidArgumentException implements ContainerExceptionInterface
+final class ServiceNameMustBeNonEmptyStringException extends InvalidArgumentException implements ExceptionInterface
 {
 }
