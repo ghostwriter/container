40136ce80f806543fee9b8b5e9c099f0d61f56f4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 16:00:03 -0600

    Update DontSerializeContainerException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/DontSerializeContainerException.php
+++ src/Exception/DontSerializeContainerException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container\Exception;
 
-use Ghostwriter\Container\Interface\ContainerExceptionInterface;
+use Ghostwriter\Container\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class DontSerializeContainerException extends RuntimeException implements ContainerExceptionInterface
+final class DontSerializeContainerException extends RuntimeException implements ExceptionInterface
 {
 }
