1fb5b801a853d627a307e70aec9b78f064c62927
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 16:15:01 -0600

    Create ServiceProviderMustBeAnInstanceOfServiceProviderInterfaceExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/ServiceProviderMustBeAnInstanceOfServiceProviderInterfaceExceptionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\Tests\Unit\Exception;
+
+use Ghostwriter\Container\Container;
+use Ghostwriter\Container\Exception\ServiceProviderMustBeAnInstanceOfServiceProviderInterfaceException;
+use Ghostwriter\Container\Instantiator;
+use Ghostwriter\Container\Interface\ServiceProviderInterface;
+use Ghostwriter\Container\ParameterBuilder;
+use Ghostwriter\Container\Reflector;
+use Ghostwriter\Container\Tests\Unit\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Throwable;
+
+#[CoversClass(ServiceProviderMustBeAnInstanceOfServiceProviderInterfaceException::class)]
+#[CoversClass(Container::class)]
+#[CoversClass(Instantiator::class)]
+#[CoversClass(ParameterBuilder::class)]
+#[CoversClass(Reflector::class)]
+final class ServiceProviderMustBeAnInstanceOfServiceProviderInterfaceExceptionTest extends AbstractTestCase
+{
+    /**
+     * @throws Throwable
+     */
+    public function testContainerProvide(): void
+    {
+        $this->assertException(ServiceProviderMustBeAnInstanceOfServiceProviderInterfaceException::class);
+        $this->expectExceptionMessage(ServiceProviderInterface::class);
+
+        Container::getInstance()->provide(ServiceProviderInterface::class);
+    }
+}
