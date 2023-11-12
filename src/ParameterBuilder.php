bb708495478ea4782635fd1ffacbc0d335ffb7ca
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 16:07:52 -0600

    Update ParameterBuilder.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ParameterBuilder.php
+++ src/ParameterBuilder.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Ghostwriter\Container;
 
 use Ghostwriter\Container\Exception\UnresolvableParameterException;
-use Ghostwriter\Container\Interface\ContainerExceptionInterface;
+use Ghostwriter\Container\Interface\ExceptionInterface;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\Exception\NotFoundExceptionInterface;
 use ReflectionException;
 use ReflectionNamedType;
 use ReflectionParameter;
 use Throwable;
+use function array_key_exists;
+use function array_key_first;
+use function array_values;
+use function is_callable;
 
 final readonly class ParameterBuilder
 {
@@ -19,64 +23,67 @@ final readonly class ParameterBuilder
      * @template TArgument
      *
      * @param array<ReflectionParameter> $parameters
-     * @param array<TArgument>           $arguments
-     *
-     * @throws NotFoundExceptionInterface
-     * @throws ReflectionException
-     * @throws Throwable
-     * @throws ContainerExceptionInterface
+     * @param array<TArgument> $arguments
      *
      * @return array<TArgument>
+     * @throws ReflectionException
+     * @throws Throwable
+     * @throws ExceptionInterface
+     *
+     * @throws NotFoundExceptionInterface
      */
     public function build(
         ContainerInterface $container,
-        array $parameters = [],
-        array $arguments = []
-    ): array {
-        return [...array_map(
-            /**
-             * @throws ContainerExceptionInterface
-             * @throws NotFoundExceptionInterface
-             * @throws ReflectionException
-             * @throws Throwable
-             */
-            static function (ReflectionParameter $reflectionParameter) use ($container, &$arguments) {
-                $parameterName = $reflectionParameter->getName();
-                if ($arguments !== []) {
-                    $parameterKey = array_key_exists($parameterName, $arguments) ?
-                        $parameterName :
-                        array_key_first($arguments);
+        array              $parameters = [],
+        array              $arguments = []
+    ): array
+    {
+        /** @var array<TArgument> */
+        return [
+            ...array_map(
+            /** @throws Throwable */
+                static function (ReflectionParameter $reflectionParameter) use ($container, &$arguments) {
+                    $parameterName = $reflectionParameter->getName();
+                    if ($arguments !== []) {
+                        /** @var class-string<TArgument> $parameterKey */
+                        $parameterKey = array_key_exists($parameterName, $arguments) ?
+                            $parameterName :
+                            array_key_first($arguments);
 
-                    $argument = $arguments[$parameterKey];
+                        /** @var TArgument $argument */
+                        $argument = $arguments[$parameterKey];
 
-                    unset($arguments[$parameterKey]);
+                        unset($arguments[$parameterKey]);
 
-                    return $argument;
-                }
+                        return $argument;
+                    }
 
-                $reflectionType = $reflectionParameter->getType();
-                if ($reflectionType instanceof ReflectionNamedType && ! $reflectionType->isBuiltin()) {
-                    return $container->get($reflectionType->getName());
-                }
+                    $reflectionType = $reflectionParameter->getType();
+                    if ($reflectionType instanceof ReflectionNamedType && !$reflectionType->isBuiltin()) {
+                        /** @var TArgument */
+                        return $container->get($reflectionType->getName());
+                    }
 
-                if ($reflectionParameter->isDefaultValueAvailable()) {
-                    return $reflectionParameter->getDefaultValue();
-                }
+                    if ($reflectionParameter->isDefaultValueAvailable()) {
+                        /** @var TArgument */
+                        return $reflectionParameter->getDefaultValue();
+                    }
 
-                $name = $reflectionParameter->getDeclaringFunction()->getName();
+                    $name = $reflectionParameter->getDeclaringFunction()->getName();
 
-                $isFunction = is_callable($name);
+                    $isFunction = is_callable($name);
 
-                throw new UnresolvableParameterException(sprintf(
-                    'Unresolvable %s parameter "$%s" in "%s%s()"; does not have a default value.',
-                    $isFunction ? 'function' : 'class',
-                    $parameterName,
-                    $isFunction ? $name : $reflectionParameter->getDeclaringClass()?->getName(),
-                    $isFunction ? '' : '::' . $name
-                ));
-            },
-            $parameters
-        ),
-            ...array_values($arguments)];
+                    throw new UnresolvableParameterException(sprintf(
+                        'Unresolvable %s parameter "$%s" in "%s%s()"; does not have a default value.',
+                        $isFunction ? 'function' : 'class',
+                        $parameterName,
+                        $isFunction ? $name : $reflectionParameter->getDeclaringClass()?->getName(),
+                        $isFunction ? '' : '::' . $name
+                    ));
+                },
+                $parameters
+            ),
+            ...array_values($arguments)
+        ];
     }
 }
