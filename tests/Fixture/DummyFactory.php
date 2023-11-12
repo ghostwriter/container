1b82145fc5b58d16d7b9f4d35bc2b385593f501d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 16:09:21 -0600

    Update DummyFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/DummyFactory.php
+++ tests/Fixture/DummyFactory.php
@@ -6,14 +6,14 @@ namespace Ghostwriter\Container\Tests\Fixture;
 
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\Exception\NotFoundExceptionInterface;
-use Ghostwriter\Container\Interface\ContainerExceptionInterface;
+use Ghostwriter\Container\Interface\ExceptionInterface;
 use Ghostwriter\Container\Interface\FactoryInterface;
 
 final readonly class DummyFactory implements FactoryInterface
 {
     /**
      * @throws NotFoundExceptionInterface
-     * @throws ContainerExceptionInterface
+     * @throws ExceptionInterface
      */
     public function __invoke(ContainerInterface $container, array $arguments = []): DummyInterface
     {
