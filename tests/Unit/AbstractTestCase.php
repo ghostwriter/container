bc21f92000606bdc14098586d8b0c39afd4d7c1f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 13:11:35 -0600

    Update AbstractTestCase.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/AbstractTestCase.php
+++ tests/Unit/AbstractTestCase.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container\Tests\Unit;
 
+use Ghostwriter\Container\Container;
 use PHPUnit\Framework\TestCase;
+use Throwable;
 
 abstract class AbstractTestCase extends TestCase
 {
+    /**
+     * @throws Throwable
+     */
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        Container::getInstance()->__destruct();
+    }
 }
