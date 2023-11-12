56a66e8e0e2dce46a0fe0d4b82a7f658217a315a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 16:15:19 -0600

    Create ServiceTagNotFoundExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/ServiceTagNotFoundExceptionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\Tests\Unit\Exception;
+
+use Ghostwriter\Container\Container;
+use Ghostwriter\Container\Exception\ServiceTagNotFoundException;
+use Ghostwriter\Container\Instantiator;
+use Ghostwriter\Container\ParameterBuilder;
+use Ghostwriter\Container\Reflector;
+use Ghostwriter\Container\Tests\Unit\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Throwable;
+
+#[CoversClass(ServiceTagNotFoundException::class)]
+#[CoversClass(Container::class)]
+#[CoversClass(Instantiator::class)]
+#[CoversClass(ParameterBuilder::class)]
+#[CoversClass(Reflector::class)]
+final class ServiceTagNotFoundExceptionTest extends AbstractTestCase
+{
+    /**
+     * @throws Throwable
+     */
+    public function testContainerTagged(): void
+    {
+        $this->assertNotFoundException(ServiceTagNotFoundException::class);
+
+        iterator_to_array(Container::getInstance()->tagged('tag-not-found'));
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testContainerUntag(): void
+    {
+        $this->assertNotFoundException(ServiceTagNotFoundException::class);
+
+        Container::getInstance()->untag(self::class, ['tag-not-found']);
+    }
+}
