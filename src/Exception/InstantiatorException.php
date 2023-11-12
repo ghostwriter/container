a5f32618b3fde741c95fff6cc0a1b3180789132b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 16:00:11 -0600

    Update InstantiatorException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/InstantiatorException.php
+++ src/Exception/InstantiatorException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container\Exception;
 
-use Ghostwriter\Container\Interface\ContainerExceptionInterface;
+use Ghostwriter\Container\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class InstantiatorException extends InvalidArgumentException implements ContainerExceptionInterface
+final class InstantiatorException extends InvalidArgumentException implements ExceptionInterface
 {
 }
