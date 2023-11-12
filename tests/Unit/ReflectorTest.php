179adca8e7ad35568a10ac99489dd33d688f52be
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 13:46:54 -0600

    Update ReflectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/ReflectorTest.php
+++ tests/Unit/ReflectorTest.php
@@ -4,55 +4,44 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container\Tests\Unit;
 
-use Ghostwriter\Container\Exception\ReflectorException;
-use Ghostwriter\Container\Interface\ContainerExceptionInterface;
+use Ghostwriter\Container\Container;
+use Ghostwriter\Container\Instantiator;
+use Ghostwriter\Container\ParameterBuilder;
 use Ghostwriter\Container\Reflector;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\UsesClass;
+use Throwable;
 
+/**
+ * @see Reflector
+ */
+#[CoversClass(Container::class)]
+#[CoversClass(Instantiator::class)]
+#[CoversClass(ParameterBuilder::class)]
 #[CoversClass(Reflector::class)]
-#[UsesClass(ReflectorException::class)]
 final class ReflectorTest extends AbstractTestCase
 {
-    private Reflector $reflector;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->reflector = new Reflector();
-    }
-
+    /**
+     * @throws Throwable
+     */
     public function testReflectClass(): void
     {
         self::assertSame(
             self::class,
-            $this->reflector->reflectClass(self::class)->getName()
+            (new Reflector())
+                ->reflectClass(self::class)
+                ->getName()
         );
     }
 
-    public function testReflectClassThrowsReflectorException(): void
-    {
-        $this->expectException(ContainerExceptionInterface::class);
-        $this->expectException(ReflectorException::class);
-        $this->expectExceptionMessage('Class "dose-not-exist" does not exist');
-
-        $this->reflector->reflectClass('dose-not-exist');
-    }
-
+    /**
+     * @throws Throwable
+     */
     public function testReflectFunction(): void
     {
         self::assertTrue(
-            $this->reflector->reflectFunction(static fn (): null => null)->isStatic()
+            (new Reflector())
+                ->reflectFunction(static fn(): null => null)
+                ->isStatic()
         );
-    }
-
-    public function testReflectFunctionThrowsReflectorException(): void
-    {
-        $this->expectException(ContainerExceptionInterface::class);
-        $this->expectException(ReflectorException::class);
-        $this->expectExceptionMessage('Function dose-not-exist() does not exist');
-
-        $this->reflector->reflectFunction('dose-not-exist');
     }
 }
