26cb0e950651b279ace476bf21c373886fdef4cc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 15:59:59 -0600

    Update DontCloneContainerException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/DontCloneContainerException.php
+++ src/Exception/DontCloneContainerException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container\Exception;
 
-use Ghostwriter\Container\Interface\ContainerExceptionInterface;
+use Ghostwriter\Container\Interface\ExceptionInterface;
 use RuntimeException;
 
-final class DontCloneContainerException extends RuntimeException implements ContainerExceptionInterface
+final class DontCloneContainerException extends RuntimeException implements ExceptionInterface
 {
 }
