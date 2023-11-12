ca3a23511ae37e36c269ff55e86a3f7091e14ea9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-12 13:13:44 -0600

    Create AliasNameAndServiceNameCannotBeTheSameExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/AliasNameAndServiceNameCannotBeTheSameExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\Tests\Unit\Exception;
+
+use Ghostwriter\Container\Container;
+use Ghostwriter\Container\Exception\AliasNameAndServiceNameCannotBeTheSameException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use stdClass;
+use Throwable;
+
+#[CoversClass(AliasNameAndServiceNameCannotBeTheSameException::class)]
+#[UsesClass(Container::class)]
+final class AliasNameAndServiceNameCannotBeTheSameExceptionTest extends AbstractExceptionTestCase
+{
+    /**
+     * @throws Throwable
+     */
+    public function testContainerAlias(): void
+    {
+        $this->assertConainerExceptionInterface(AliasNameAndServiceNameCannotBeTheSameException::class);
+
+        Container::getInstance()
+                 ->alias(stdClass::class, stdClass::class);
+    }
+}
