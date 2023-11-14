f44c059d2312a08880a8c15c446120c1cf756d52
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-14 09:26:26 -0600

    Throw if factories do not return an object - Closes #36
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/ServiceMustBeAnObjectExceptionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\Tests\Unit\Exception;
+
+use Ghostwriter\Container\Container;
+use Ghostwriter\Container\Exception\ServiceMustBeAnObjectException;
+use Ghostwriter\Container\Instantiator;
+use Ghostwriter\Container\ParameterBuilder;
+use Ghostwriter\Container\Reflector;
+use Ghostwriter\Container\Tests\Fixture\NonStdClassFactory;
+use Ghostwriter\Container\Tests\Unit\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use stdClass;
+use Throwable;
+
+#[CoversClass(ServiceMustBeAnObjectException::class)]
+#[CoversClass(Container::class)]
+#[CoversClass(Instantiator::class)]
+#[CoversClass(ParameterBuilder::class)]
+#[CoversClass(Reflector::class)]
+final class ServiceMustBeAnObjectExceptionTest extends AbstractTestCase
+{
+    /**
+     * @throws Throwable
+     */
+    public function testContainerBuild(): void
+    {
+        $this->assertException(ServiceMustBeAnObjectException::class);
+
+        $container = Container::getInstance();
+
+        $container->set(stdClass::class, static fn() => false);
+
+        $container->build(stdClass::class);
+    }
+}
