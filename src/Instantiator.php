9077cac2bae1b4342a289e5c8818221b30ab9cfb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-29 02:10:31 -0600

    Update Instantiator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Instantiator.php
+++ src/Instantiator.php
@@ -4,76 +4,17 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Container;
 
-use Closure;
 use Ghostwriter\Container\Exception\ClassNotInstantiableException;
 use Ghostwriter\Container\Exception\InstantiatorException;
-use Ghostwriter\Container\Interface\ContainerInterface;
 use Throwable;
-use ReflectionClass;
-use function sprintf;
 
-/** @see Ghostwriter\Container\Tests\Unit\InstantiatorTest */
+/** @see \Ghostwriter\ContainerTests\Unit\InstantiatorTest */
 final readonly class Instantiator
 {
     public function __construct(
-        private Reflector $reflector = new Reflector(),
-        private ParameterBuilder $parameterBuilder = new ParameterBuilder(),
+        public Reflector $reflector,
+        public ParameterBuilder $parameterBuilder,
     ) {
-    }
-
-    /**
-     * @template TService of object
-     * @template TArgument
-     *
-     * @param array<TArgument> $arguments
-     *
-     * @return TService
-     * @param Closure(): void $function
-     */
-    public function buildParameters(
-        ContainerInterface $container,
-        Closure $function,
-        array $arguments = []
-    ): array {
-        $parameters = $this->reflector
-            ->reflectFunction($function)
-            ->getParameters();
-
-        return $this->parameterBuilder->build(
-            $container,
-            $parameters,
-            $arguments
-        );
-    }
-
-    /**
-     * @template TService of object
-     * @template TArgument
-     *
-     * @param array<TArgument> $arguments
-     *
-     * @return TService
-     * @param Closure(): void $function
-     */
-    private function buildClassParameters(
-        ContainerInterface $container,
-        string $class,
-        array $arguments = []
-    ): array {
-        $reflectionClass =  $this->cachedReflectionClass($container, $class);
-
-        if (!$reflectionClass->isInstantiable()) {
-            throw new ClassNotInstantiableException($class);
-        }
-
-        return [
-            $reflectionClass,
-            $this->parameterBuilder->build(
-                $container,
-                $reflectionClass->getConstructor()?->getParameters() ?? [],
-                $arguments
-            )
-        ];
     }
 
     /**
@@ -86,11 +27,19 @@ final readonly class Instantiator
      * @return TService
      */
     public function instantiate(
-        ContainerInterface $container,
         string $class,
         array $arguments = []
     ): object {
-        [$reflectionClass, $parameters] = $this->buildClassParameters($container, $class, $arguments);
+        $reflectionClass = $this->reflector->reflectClass($class);
+
+        if (! $reflectionClass->isInstantiable()) {
+            throw new ClassNotInstantiableException($class);
+        }
+
+        $parameters = $this->parameterBuilder->build(
+            $reflectionClass->getConstructor()?->getParameters() ?? [],
+            $arguments
+        );
 
         try {
             return $reflectionClass->newInstance(...$parameters);
@@ -99,19 +48,10 @@ final readonly class Instantiator
         }
     }
 
-
-    private function cachedReflectionClass(ContainerInterface $container, string $class): ReflectionClass
-    {
-        $cacheKey = sprintf('%s\%s', ReflectionClass::class, $class);
-
-        if ($container->has($cacheKey)) {
-            return $container->get($cacheKey);
-        }
-
-        $reflectionClass = $this->reflector->reflectClass($class);
-
-        $container->set($cacheKey, $reflectionClass);
-
-        return $reflectionClass;
+    public static function new(
+        Reflector $reflector,
+        ParameterBuilder $parameterBuilder
+    ): self {
+        return new self($reflector, $parameterBuilder);
     }
 }
