7c2878106b6ded7e2890f4c3c5fc4a5d97dad1a2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 07:09:30 -0500

    Update ReflectorException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ReflectorException.php
+++ src/Exception/ReflectorException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\Container\Exception;
 use Ghostwriter\Container\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class ReflectorException extends InvalidArgumentException implements ExceptionInterface
-{
-}
+final class ReflectorException extends InvalidArgumentException implements ExceptionInterface {}
