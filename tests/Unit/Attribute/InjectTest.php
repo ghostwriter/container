44849564cfbf6649c658a97e41c86b9097adfd72
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:29 -0500

    Add InjectTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Attribute/InjectTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Attribute;
+
+use Ghostwriter\Container\Attribute\Extension;
+use Ghostwriter\Container\Attribute\Factory;
+use Ghostwriter\Container\Attribute\Inject;
+use Ghostwriter\Container\Container;
+use Ghostwriter\Container\List\Aliases;
+use Ghostwriter\Container\List\Bindings;
+use Ghostwriter\Container\List\Builders;
+use Ghostwriter\Container\List\Dependencies;
+use Ghostwriter\Container\List\Extensions;
+use Ghostwriter\Container\List\Factories;
+use Ghostwriter\Container\List\Instances;
+use Ghostwriter\Container\List\Providers;
+use Ghostwriter\Container\List\Tags;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Fixture\Attribute\ClassParameterHasInjectAttribute;
+use Tests\Fixture\GitHubClient;
+use Tests\Unit\AbstractTestCase;
+
+#[CoversClass(Aliases::class)]
+#[CoversClass(Bindings::class)]
+#[CoversClass(Builders::class)]
+#[CoversClass(Container::class)]
+#[CoversClass(Dependencies::class)]
+#[CoversClass(Extension::class)]
+#[CoversClass(Extensions::class)]
+#[CoversClass(Factories::class)]
+#[CoversClass(Factory::class)]
+#[CoversClass(Inject::class)]
+#[CoversClass(Instances::class)]
+#[CoversClass(Providers::class)]
+#[CoversClass(Tags::class)]
+final class InjectTest extends AbstractTestCase
+{
+    public function testClassParameterHasInjectAttribute(): void
+    {
+        self::assertInstanceOf(
+            GitHubClient::class,
+            $this->container->get(ClassParameterHasInjectAttribute::class)->getClient()
+        );
+    }
+}
