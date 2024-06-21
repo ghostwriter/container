60871d013f717c81886e2e0c3770b47987ff55a5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:28 -0500

    Add StdClassFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/Factory/StdClassFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixture\Factory;
+
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\FactoryInterface;
+use Override;
+use stdClass;
+
+final readonly class StdClassFactory implements FactoryInterface
+{
+    #[Override]
+    public function __invoke(ContainerInterface $container): stdClass
+    {
+        $object = new stdClass();
+
+        $object->blackLivesMatter = '#FreePalestine';
+
+        return $object;
+    }
+}
