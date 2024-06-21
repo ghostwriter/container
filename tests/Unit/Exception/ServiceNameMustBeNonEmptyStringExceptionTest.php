9e833ba645ecc379ede73ab031f04d19aa51003c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:30 -0500

    Update ServiceNameMustBeNonEmptyStringExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/ServiceNameMustBeNonEmptyStringExceptionTest.php
+++ tests/Unit/Exception/ServiceNameMustBeNonEmptyStringExceptionTest.php
@@ -2,25 +2,48 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Unit\Exception;
+namespace Tests\Unit\Exception;
 
+use Ghostwriter\Container\Attribute\Extension;
+use Ghostwriter\Container\Attribute\Factory;
+use Ghostwriter\Container\Attribute\Inject;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Container\Exception\ServiceNameMustBeNonEmptyStringException;
-use Ghostwriter\Container\Instantiator;
-use Ghostwriter\Container\ParameterBuilder;
-use Ghostwriter\Container\Reflector;
-use Ghostwriter\ContainerTests\Fixture\Extension\StdClassOneExtension;
-use Ghostwriter\ContainerTests\Fixture\StdClassFactory;
-use Ghostwriter\ContainerTests\Unit\AbstractTestCase;
+use Ghostwriter\Container\List\Aliases;
+use Ghostwriter\Container\List\Bindings;
+use Ghostwriter\Container\List\Builders;
+use Ghostwriter\Container\List\Dependencies;
+use Ghostwriter\Container\List\Extensions;
+use Ghostwriter\Container\List\Factories;
+use Ghostwriter\Container\List\Instances;
+use Ghostwriter\Container\List\Providers;
+use Ghostwriter\Container\List\Tags;
 use PHPUnit\Framework\Attributes\CoversClass;
 use stdClass;
+use Tests\Fixture\Extension\StdClassOneExtension;
+use Tests\Fixture\Factory\StdClassFactory;
+use Tests\Unit\AbstractTestCase;
 use Throwable;
 
+/**
+ * @psalm-suppress ArgumentTypeCoercion
+ * @psalm-suppress InvalidArgument
+ * @psalm-suppress UndefinedClass
+ */
 #[CoversClass(ServiceNameMustBeNonEmptyStringException::class)]
+#[CoversClass(Aliases::class)]
+#[CoversClass(Bindings::class)]
+#[CoversClass(Builders::class)]
 #[CoversClass(Container::class)]
-#[CoversClass(Instantiator::class)]
-#[CoversClass(ParameterBuilder::class)]
-#[CoversClass(Reflector::class)]
+#[CoversClass(Dependencies::class)]
+#[CoversClass(Extension::class)]
+#[CoversClass(Extensions::class)]
+#[CoversClass(Factories::class)]
+#[CoversClass(Factory::class)]
+#[CoversClass(Inject::class)]
+#[CoversClass(Instances::class)]
+#[CoversClass(Providers::class)]
+#[CoversClass(Tags::class)]
 final class ServiceNameMustBeNonEmptyStringExceptionTest extends AbstractTestCase
 {
     /**
@@ -30,7 +53,7 @@ final class ServiceNameMustBeNonEmptyStringExceptionTest extends AbstractTestCas
     {
         $this->assertException(ServiceNameMustBeNonEmptyStringException::class);
 
-        $this->container->alias(stdClass::class, '');
+        $this->container->alias('', stdClass::class);
     }
 
     /**
@@ -40,7 +63,7 @@ final class ServiceNameMustBeNonEmptyStringExceptionTest extends AbstractTestCas
     {
         $this->assertException(ServiceNameMustBeNonEmptyStringException::class);
 
-        $this->container->alias(stdClass::class, ' ');
+        $this->container->alias(' ', stdClass::class);
     }
 
     /**
@@ -245,6 +268,7 @@ final class ServiceNameMustBeNonEmptyStringExceptionTest extends AbstractTestCas
 
     /**
      * @throws Throwable
+     *
      */
     public function testContainerSet(): void
     {
@@ -255,6 +279,7 @@ final class ServiceNameMustBeNonEmptyStringExceptionTest extends AbstractTestCas
 
     /**
      * @throws Throwable
+     *
      */
     public function testContainerSetEmptySpace(): void
     {
