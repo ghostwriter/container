8f5b92122fbe0051889c77487e1a6b72c7c6caf2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:28 -0500

    Add ClassWithInjectAttribute.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/Attribute/ClassWithInjectAttribute.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixture\Attribute;
+
+use Ghostwriter\Container\Attribute\Inject;
+use Tests\Fixture\ClientInterface;
+use Tests\Fixture\GitHubClient;
+
+final readonly class ClassWithInjectAttribute
+{
+    public function __construct(
+        #[Inject(GitHubClient::class)]
+        private ClientInterface $client
+    ) {
+    }
+
+    public function getClient(): ClientInterface
+    {
+        return $this->client;
+    }
+}
