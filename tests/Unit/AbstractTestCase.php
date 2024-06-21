44f36d94ce31be6d8821ab2fb07dd674c8f96620
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:31 -0500

    Update AbstractTestCase.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/AbstractTestCase.php
+++ tests/Unit/AbstractTestCase.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Unit;
+namespace Tests\Unit;
 
 use Ghostwriter\Container\Container;
-use Ghostwriter\Container\Instantiator;
 use Ghostwriter\Container\Interface\Exception\NotFoundExceptionInterface;
 use Ghostwriter\Container\Interface\ExceptionInterface;
-use Ghostwriter\Container\ParameterBuilder;
-use Ghostwriter\Container\Reflector;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Throwable;
@@ -18,23 +15,11 @@ abstract class AbstractTestCase extends TestCase
 {
     protected Container $container;
 
-    protected Instantiator $instantiator;
-
-    protected ParameterBuilder $parameterBuilder;
-
-    protected Reflector $reflector;
-
     final protected function setUp(): void
     {
         parent::setUp();
 
         $this->container = Container::getInstance();
-
-        $this->reflector = new Reflector();
-
-        $this->parameterBuilder = new ParameterBuilder($this->container);
-
-        $this->instantiator = Instantiator::new($this->reflector, $this->parameterBuilder);
     }
 
     /**
@@ -44,7 +29,7 @@ abstract class AbstractTestCase extends TestCase
     {
         parent::tearDown();
 
-        $this->container->__destruct();
+        $this->container->purge();
     }
 
     /**
