213974f39e7a93744f15a900af1f91edff702759
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:35 -0500

    Update ServiceMustBeAnObjectException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ServiceMustBeAnObjectException.php
+++ src/Exception/ServiceMustBeAnObjectException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Container\Exception;
 use Ghostwriter\Container\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class ServiceMustBeAnObjectException extends InvalidArgumentException implements ExceptionInterface {}
+final class ServiceMustBeAnObjectException extends InvalidArgumentException implements ExceptionInterface
+{
+}
