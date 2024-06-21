8aeb42f8588b286245533c3532d6bebac5bc423d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:33 -0500

    Update IntConstructor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Constructor/IntConstructor.php
+++ tests/Fixture/Constructor/IntConstructor.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture\Constructor;
+namespace Tests\Fixture\Constructor;
 
-class IntConstructor
+final class IntConstructor
 {
     public int $result;
 
