db3b8863c860f62950ffb987eac2405f43fb7e15
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:37 -0500

    Update AliasNameAndServiceNameCannotBeTheSameException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/AliasNameAndServiceNameCannotBeTheSameException.php
+++ src/Exception/AliasNameAndServiceNameCannotBeTheSameException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Container\Exception;
 use Ghostwriter\Container\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class AliasNameAndServiceNameCannotBeTheSameException extends InvalidArgumentException implements ExceptionInterface {}
+final class AliasNameAndServiceNameCannotBeTheSameException extends InvalidArgumentException implements ExceptionInterface
+{
+}
