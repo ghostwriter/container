fa7be72e0f7032b1075aaf8755747a7beb61469c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:33 -0500

    Update FoobarExtension.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Extension/FoobarExtension.php
+++ tests/Fixture/Extension/FoobarExtension.php
@@ -2,16 +2,28 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture\Extension;
+namespace Tests\Fixture\Extension;
 
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\ExtensionInterface;
-use Ghostwriter\ContainerTests\Fixture\Bar;
-use Ghostwriter\ContainerTests\Fixture\Foo;
+use Override;
+use Tests\Fixture\Bar;
+use Tests\Fixture\Foo;
+use Tests\Fixture\Foobar;
+use Throwable;
 
-class FoobarExtension implements ExtensionInterface
+/**
+ * @implements ExtensionInterface<Foobar>
+ */
+final readonly class FoobarExtension implements ExtensionInterface
 {
-    public function __invoke(ContainerInterface $container, object $service): object
+    /**
+     * @param Foobar $service
+     *
+     * @throws Throwable
+     */
+    #[Override]
+    public function __invoke(ContainerInterface $container, object $service): Foobar
     {
         $service->foo = $container->get(Foo::class);
 
