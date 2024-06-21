a84738026800d3f040da952f08748e803c955c5d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:30 -0500

    Update ReflectorExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/ReflectorExceptionTest.php
+++ tests/Unit/Exception/ReflectorExceptionTest.php
@@ -2,22 +2,44 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Unit\Exception;
+namespace Tests\Unit\Exception;
 
+use Ghostwriter\Container\Attribute\Extension;
+use Ghostwriter\Container\Attribute\Factory;
+use Ghostwriter\Container\Attribute\Inject;
 use Ghostwriter\Container\Container;
-use Ghostwriter\Container\Exception\ReflectorException;
-use Ghostwriter\Container\Instantiator;
-use Ghostwriter\Container\ParameterBuilder;
-use Ghostwriter\Container\Reflector;
-use Ghostwriter\ContainerTests\Unit\AbstractTestCase;
+use Ghostwriter\Container\Exception\ReflectionException;
+use Ghostwriter\Container\List\Aliases;
+use Ghostwriter\Container\List\Bindings;
+use Ghostwriter\Container\List\Builders;
+use Ghostwriter\Container\List\Dependencies;
+use Ghostwriter\Container\List\Extensions;
+use Ghostwriter\Container\List\Factories;
+use Ghostwriter\Container\List\Instances;
+use Ghostwriter\Container\List\Providers;
+use Ghostwriter\Container\List\Tags;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Unit\AbstractTestCase;
 use Throwable;
 
-#[CoversClass(ReflectorException::class)]
+/**
+ * @psalm-suppress ArgumentTypeCoercion
+ * @psalm-suppress UndefinedClass
+ */
+#[CoversClass(ReflectionException::class)]
+#[CoversClass(Aliases::class)]
+#[CoversClass(Bindings::class)]
+#[CoversClass(Builders::class)]
 #[CoversClass(Container::class)]
-#[CoversClass(Instantiator::class)]
-#[CoversClass(ParameterBuilder::class)]
-#[CoversClass(Reflector::class)]
+#[CoversClass(Dependencies::class)]
+#[CoversClass(Extension::class)]
+#[CoversClass(Extensions::class)]
+#[CoversClass(Factories::class)]
+#[CoversClass(Factory::class)]
+#[CoversClass(Inject::class)]
+#[CoversClass(Instances::class)]
+#[CoversClass(Providers::class)]
+#[CoversClass(Tags::class)]
 final class ReflectorExceptionTest extends AbstractTestCase
 {
     /**
@@ -25,34 +47,34 @@ final class ReflectorExceptionTest extends AbstractTestCase
      */
     public function testContainerBuild(): void
     {
-        $this->assertException(ReflectorException::class);
+        $this->assertException(ReflectionException::class);
         $this->expectExceptionMessage('Class "does-not-exist" does not exist');
-        $this->expectExceptionCode(0);
+        $this->expectExceptionCode(127);
 
         $this->container->build('does-not-exist');
     }
 
-    /**
-     * @throws Throwable
-     */
-    public function testReflectorReflectClass(): void
-    {
-        $this->assertException(ReflectorException::class);
-        $this->expectExceptionMessage('Class "dose-not-exist" does not exist');
-        $this->expectExceptionCode(0);
-
-        $this->reflector->reflectClass('dose-not-exist');
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testReflectorReflectFunction(): void
-    {
-        $this->assertException(ReflectorException::class);
-        $this->expectExceptionMessage('Function dose-not-exist() does not exist');
-        $this->expectExceptionCode(0);
-
-        $this->reflector->reflectFunction('dose-not-exist');
-    }
+    //    /**
+    //     * @throws Throwable
+    //     */
+    //    public function testReflectorReflectClass(): void
+    //    {
+    //        $this->assertException(ReflectionException::class);
+    //        $this->expectExceptionMessage('Class "dose-not-exist" does not exist');
+    //        $this->expectExceptionCode(127);
+    //
+    //        $this->container->reflectClass('dose-not-exist');
+    //    }
+    //
+    //    /**
+    //     * @throws Throwable
+    //     */
+    //    public function testReflectorReflectFunction(): void
+    //    {
+    //        $this->assertException(ReflectionException::class);
+    //        $this->expectExceptionMessage('Function dose-not-exist() does not exist');
+    //        $this->expectExceptionCode(127);
+    //
+    //        $this->container->reflectFunction('dose-not-exist');
+    //    }
 }
