4cc5d6bc7a2c9b70808e55141c3c55fac32d60ec
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:40 -0500

    Add ReflectionException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/ReflectionException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\Exception;
+
+use Ghostwriter\Container\Interface\ExceptionInterface;
+
+final class ReflectionException extends \ReflectionException implements ExceptionInterface
+{
+}
