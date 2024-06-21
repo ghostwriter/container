593b4ff91149e68196b42155eea8b02a9e00eab8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:39 -0500

    Add ClassParameterHasExtensionAttributeExtension.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/Extension/ClassParameterHasExtensionAttributeExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixture\Extension;
+
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\ExtensionInterface;
+use Override;
+use Tests\Fixture\Attribute\ClassParameterHasExtensionAttribute;
+use Tests\Fixture\Foobar;
+
+/**
+ * @implements ExtensionInterface<ClassParameterHasExtensionAttribute>
+ */
+final readonly class ClassParameterHasExtensionAttributeExtension implements ExtensionInterface
+{
+    /**
+     * @param ClassParameterHasExtensionAttribute $service
+     */
+    #[Override] public function __invoke(ContainerInterface $container, object $service): ClassParameterHasExtensionAttribute
+    {
+        $service->setFoobar(new Foobar(time()));
+
+        return $service;
+    }
+}
