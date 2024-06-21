1d6aa782b2a42c4546eb49abffc65816eee0460e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:35 -0500

    Update ServiceFactoryMustBeAnInstanceOfFactoryInterfaceException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ServiceFactoryMustBeAnInstanceOfFactoryInterfaceException.php
+++ src/Exception/ServiceFactoryMustBeAnInstanceOfFactoryInterfaceException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Container\Exception;
 use Ghostwriter\Container\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class ServiceFactoryMustBeAnInstanceOfFactoryInterfaceException extends InvalidArgumentException implements ExceptionInterface {}
+final class ServiceFactoryMustBeAnInstanceOfFactoryInterfaceException extends InvalidArgumentException implements ExceptionInterface
+{
+}
