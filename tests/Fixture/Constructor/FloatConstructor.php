8a142848b0f7caa4a6586ef73a5a3b92aad3e065
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:33 -0500

    Update FloatConstructor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Constructor/FloatConstructor.php
+++ tests/Fixture/Constructor/FloatConstructor.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture\Constructor;
+namespace Tests\Fixture\Constructor;
 
-class FloatConstructor
+final class FloatConstructor
 {
     public float $result;
 
