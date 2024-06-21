5a7e5bb9a39f25661bb0013fabb1bf48d8c687b5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:29 -0500

    Update ServiceTagNotFoundExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/ServiceTagNotFoundExceptionTest.php
+++ tests/Unit/Exception/ServiceTagNotFoundExceptionTest.php
@@ -2,24 +2,42 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Unit\Exception;
+namespace Tests\Unit\Exception;
 
+use Ghostwriter\Container\Attribute\Extension;
+use Ghostwriter\Container\Attribute\Factory;
+use Ghostwriter\Container\Attribute\Inject;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Container\Exception\ServiceTagNotFoundException;
-use Ghostwriter\Container\Instantiator;
-use Ghostwriter\Container\ParameterBuilder;
-use Ghostwriter\Container\Reflector;
-use Ghostwriter\ContainerTests\Unit\AbstractTestCase;
+use Ghostwriter\Container\List\Aliases;
+use Ghostwriter\Container\List\Bindings;
+use Ghostwriter\Container\List\Builders;
+use Ghostwriter\Container\List\Dependencies;
+use Ghostwriter\Container\List\Extensions;
+use Ghostwriter\Container\List\Factories;
+use Ghostwriter\Container\List\Instances;
+use Ghostwriter\Container\List\Providers;
+use Ghostwriter\Container\List\Tags;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Unit\AbstractTestCase;
 use Throwable;
 
 use function iterator_to_array;
 
 #[CoversClass(ServiceTagNotFoundException::class)]
+#[CoversClass(Aliases::class)]
+#[CoversClass(Bindings::class)]
+#[CoversClass(Builders::class)]
 #[CoversClass(Container::class)]
-#[CoversClass(Instantiator::class)]
-#[CoversClass(ParameterBuilder::class)]
-#[CoversClass(Reflector::class)]
+#[CoversClass(Dependencies::class)]
+#[CoversClass(Extension::class)]
+#[CoversClass(Extensions::class)]
+#[CoversClass(Factories::class)]
+#[CoversClass(Factory::class)]
+#[CoversClass(Inject::class)]
+#[CoversClass(Instances::class)]
+#[CoversClass(Providers::class)]
+#[CoversClass(Tags::class)]
 final class ServiceTagNotFoundExceptionTest extends AbstractTestCase
 {
     /**
@@ -29,7 +47,7 @@ final class ServiceTagNotFoundExceptionTest extends AbstractTestCase
     {
         $this->assertNotFoundException(ServiceTagNotFoundException::class);
 
-        iterator_to_array($this->container->tagged('tag-not-found'));
+        iterator_to_array($this->container->tagged(self::class));
     }
 
     /**
@@ -39,6 +57,6 @@ final class ServiceTagNotFoundExceptionTest extends AbstractTestCase
     {
         $this->assertNotFoundException(ServiceTagNotFoundException::class);
 
-        $this->container->untag(self::class, ['tag-not-found']);
+        $this->container->untag(self::class, [self::class]);
     }
 }
