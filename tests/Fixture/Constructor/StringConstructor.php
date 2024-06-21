1f2302a85201ca006d991c4a74eaabee6868b5cd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:33 -0500

    Update StringConstructor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Constructor/StringConstructor.php
+++ tests/Fixture/Constructor/StringConstructor.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture\Constructor;
+namespace Tests\Fixture\Constructor;
 
-class StringConstructor
+final class StringConstructor
 {
     public string $result;
 
