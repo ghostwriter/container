4f667f7ce53e5612ce028ccd06fdd23dfdb8dfe2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:28 -0500

    Add Extensions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/List/Extensions.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\List;
+
+use Ghostwriter\Container\Interface\ExtensionInterface;
+use Ghostwriter\Container\Interface\ListInterface;
+
+/**
+ * @template-covariant TService of object
+ */
+final class Extensions implements ListInterface
+{
+    /**
+     * @param array<class-string<TService>,non-empty-array<class-string<ExtensionInterface<TService>>,bool>> $list
+     */
+    public function __construct(
+        private array $list = []
+    ) {
+    }
+
+    /**
+     * @return array<class-string<TService>,array<class-string<ExtensionInterface<TService>>,bool>>
+     */
+    public function all(): array
+    {
+        return $this->list;
+    }
+
+    /**
+     * @template TSet of object
+     *
+     * @param class-string<TSet>                     $service
+     * @param class-string<ExtensionInterface<TSet>> $extension
+     */
+    public function set(string $service, string $extension): void
+    {
+        /** @var self<TService|TSet> $this */
+        $this->list[$service][$extension] = true;
+    }
+
+    /**
+     * @param class-string<TService> $service
+     */
+    public function unset(string $service): void
+    {
+        unset($this->list[$service]);
+    }
+
+    /**
+     * @template TNewService of object
+     *
+     * @param array<class-string<TNewService>,non-empty-array<class-string<ExtensionInterface<TNewService>>,bool>> $list
+     */
+    public static function new(array $list = []): self
+    {
+        return new self($list);
+    }
+}
