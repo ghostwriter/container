08e15749e86ea50f7d34db5cad68a34cc0ce7bab
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:33 -0500

    Update Dummy.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Dummy.php
+++ tests/Fixture/Dummy.php
@@ -2,24 +2,31 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture;
+namespace Tests\Fixture;
 
+use Ghostwriter\Container\Attribute\Factory;
+use Tests\Fixture\Factory\DummyFactory;
+
+use function array_values;
+use function sprintf;
+
+#[Factory(DummyFactory::class)]
 final readonly class Dummy implements DummyInterface
 {
-    public function __construct(private DummyFactory $dummyFactory)
-    {
-    }
-
-    public function __invoke(
-        array $data = ['Text'],
-        string $text = 'Untitled %s',
-    ): string {
-        return sprintf($text, ...array_values($data));
+    public function __construct(
+        private DummyFactory $dummyFactory
+    ) {
     }
 
     /**
-     * @return DummyFactory
+     * @template T of string
+     * @param array<T> $data
      */
+    public function __invoke(array $data = ['Text'], string $text = 'Untitled %s'): string
+    {
+        return sprintf($text, ...array_values($data));
+    }
+
     public function getDummyFactory(): DummyFactory
     {
         return $this->dummyFactory;
