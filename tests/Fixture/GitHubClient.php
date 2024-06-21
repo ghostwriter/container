63bce5ab50f3becabee1f997319226f8290916c2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:32 -0500

    Update GitHubClient.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/GitHubClient.php
+++ tests/Fixture/GitHubClient.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture;
+namespace Tests\Fixture;
 
-class GitHubClient implements ClientInterface
+final class GitHubClient implements ClientInterface
 {
 }
