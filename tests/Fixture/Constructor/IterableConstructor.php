b48cf6527ce1cfdd2ee4e21788e01f4bd3301ac9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:33 -0500

    Update IterableConstructor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Constructor/IterableConstructor.php
+++ tests/Fixture/Constructor/IterableConstructor.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture\Constructor;
+namespace Tests\Fixture\Constructor;
 
-class IterableConstructor
+final class IterableConstructor
 {
     public iterable $result;
 
