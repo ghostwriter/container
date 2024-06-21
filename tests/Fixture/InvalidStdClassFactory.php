77d6e77cd4365723f5683527590c6471efb852d2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:32 -0500

    Update InvalidStdClassFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/InvalidStdClassFactory.php
+++ tests/Fixture/InvalidStdClassFactory.php
@@ -2,15 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture;
+namespace Tests\Fixture;
 
 use Ghostwriter\Container\Interface\ContainerInterface;
 use stdClass;
 
-final readonly class InvalidStdClassFactory
+final readonly class InvalidStdClassFactory /* Does not implement FactoryInterface */
 {
-
-    public function __invoke(ContainerInterface $container, array $arguments = []): stdClass
+    public function __invoke(ContainerInterface $container): stdClass
     {
         return new stdClass();
     }
