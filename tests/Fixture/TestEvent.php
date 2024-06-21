f0e7c4c476c7749308c9b88fba0d6749a035d6bc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:32 -0500

    Update TestEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/TestEvent.php
+++ tests/Fixture/TestEvent.php
@@ -2,21 +2,23 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture;
+namespace Tests\Fixture;
 
-class TestEvent
+use function time;
+
+final class TestEvent
 {
     /** @var array<array-key,string> */
     private array $events = [];
-
-    public function collect(string $event): void
-    {
-        $this->events[] = $event . time();
-    }
 
     /** @return array<array-key,string> */
     public function all(): array
     {
         return $this->events;
+    }
+
+    public function collect(string $event): void
+    {
+        $this->events[] = $event . time();
     }
 }
