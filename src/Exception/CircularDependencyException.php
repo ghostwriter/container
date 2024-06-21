d5050e9c99f478439383a9373c5295ff91720980
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:36 -0500

    Update CircularDependencyException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/CircularDependencyException.php
+++ src/Exception/CircularDependencyException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Container\Exception;
 use Ghostwriter\Container\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class CircularDependencyException extends InvalidArgumentException implements ExceptionInterface {}
+final class CircularDependencyException extends InvalidArgumentException implements ExceptionInterface
+{
+}
