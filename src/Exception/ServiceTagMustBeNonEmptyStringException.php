c396df437343e69e740b119dd1e93974e0fadaff
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:34 -0500

    Update ServiceTagMustBeNonEmptyStringException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ServiceTagMustBeNonEmptyStringException.php
+++ src/Exception/ServiceTagMustBeNonEmptyStringException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Container\Exception;
 use Ghostwriter\Container\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class ServiceTagMustBeNonEmptyStringException extends InvalidArgumentException implements ExceptionInterface {}
+final class ServiceTagMustBeNonEmptyStringException extends InvalidArgumentException implements ExceptionInterface
+{
+}
