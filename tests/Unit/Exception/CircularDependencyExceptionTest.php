bcaa8909b9ce426615ff8b7fccf83342097cb9ce
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:30 -0500

    Update CircularDependencyExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/CircularDependencyExceptionTest.php
+++ tests/Unit/Exception/CircularDependencyExceptionTest.php
@@ -2,31 +2,49 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Unit\Exception;
+namespace Tests\Unit\Exception;
 
+use Ghostwriter\Container\Attribute\Extension;
+use Ghostwriter\Container\Attribute\Factory;
+use Ghostwriter\Container\Attribute\Inject;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Container\Exception\CircularDependencyException;
-use Ghostwriter\Container\Instantiator;
-use Ghostwriter\Container\ParameterBuilder;
-use Ghostwriter\Container\Reflector;
-use Ghostwriter\ContainerTests\Fixture\CircularDependency\ClassA;
-use Ghostwriter\ContainerTests\Fixture\CircularDependency\ClassB;
-use Ghostwriter\ContainerTests\Fixture\CircularDependency\ClassC;
-use Ghostwriter\ContainerTests\Fixture\CircularDependency\ClassX;
-use Ghostwriter\ContainerTests\Fixture\CircularDependency\ClassY;
-use Ghostwriter\ContainerTests\Fixture\CircularDependency\ClassZ;
-use Ghostwriter\ContainerTests\Unit\AbstractTestCase;
+use Ghostwriter\Container\List\Aliases;
+use Ghostwriter\Container\List\Bindings;
+use Ghostwriter\Container\List\Builders;
+use Ghostwriter\Container\List\Dependencies;
+use Ghostwriter\Container\List\Extensions;
+use Ghostwriter\Container\List\Factories;
+use Ghostwriter\Container\List\Instances;
+use Ghostwriter\Container\List\Providers;
+use Ghostwriter\Container\List\Tags;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Fixture\CircularDependency\ClassA;
+use Tests\Fixture\CircularDependency\ClassB;
+use Tests\Fixture\CircularDependency\ClassC;
+use Tests\Fixture\CircularDependency\ClassX;
+use Tests\Fixture\CircularDependency\ClassY;
+use Tests\Fixture\CircularDependency\ClassZ;
+use Tests\Unit\AbstractTestCase;
 use Throwable;
 
 use function implode;
 use function sprintf;
 
 #[CoversClass(CircularDependencyException::class)]
+#[CoversClass(Aliases::class)]
+#[CoversClass(Bindings::class)]
+#[CoversClass(Builders::class)]
 #[CoversClass(Container::class)]
-#[CoversClass(Instantiator::class)]
-#[CoversClass(ParameterBuilder::class)]
-#[CoversClass(Reflector::class)]
+#[CoversClass(Dependencies::class)]
+#[CoversClass(Extension::class)]
+#[CoversClass(Extensions::class)]
+#[CoversClass(Factories::class)]
+#[CoversClass(Factory::class)]
+#[CoversClass(Inject::class)]
+#[CoversClass(Instances::class)]
+#[CoversClass(Providers::class)]
+#[CoversClass(Tags::class)]
 final class CircularDependencyExceptionTest extends AbstractTestCase
 {
     /**
