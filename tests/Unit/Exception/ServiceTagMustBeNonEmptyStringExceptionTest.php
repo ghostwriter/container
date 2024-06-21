2f39fd3bf2638637cc29be673b92ceba904283fd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:29 -0500

    Update ServiceTagMustBeNonEmptyStringExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/ServiceTagMustBeNonEmptyStringExceptionTest.php
+++ tests/Unit/Exception/ServiceTagMustBeNonEmptyStringExceptionTest.php
@@ -2,34 +2,54 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Unit\Exception;
+namespace Tests\Unit\Exception;
 
+use Ghostwriter\Container\Attribute\Extension;
+use Ghostwriter\Container\Attribute\Factory;
+use Ghostwriter\Container\Attribute\Inject;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Container\Exception\ServiceTagMustBeNonEmptyStringException;
-use Ghostwriter\Container\Instantiator;
-use Ghostwriter\Container\ParameterBuilder;
-use Ghostwriter\Container\Reflector;
-use Ghostwriter\ContainerTests\Unit\AbstractTestCase;
+use Ghostwriter\Container\List\Aliases;
+use Ghostwriter\Container\List\Bindings;
+use Ghostwriter\Container\List\Builders;
+use Ghostwriter\Container\List\Dependencies;
+use Ghostwriter\Container\List\Extensions;
+use Ghostwriter\Container\List\Factories;
+use Ghostwriter\Container\List\Instances;
+use Ghostwriter\Container\List\Providers;
+use Ghostwriter\Container\List\Tags;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Unit\AbstractTestCase;
 use Throwable;
 
 use function iterator_to_array;
 
 #[CoversClass(ServiceTagMustBeNonEmptyStringException::class)]
+#[CoversClass(Aliases::class)]
+#[CoversClass(Bindings::class)]
+#[CoversClass(Builders::class)]
 #[CoversClass(Container::class)]
-#[CoversClass(Instantiator::class)]
-#[CoversClass(ParameterBuilder::class)]
-#[CoversClass(Reflector::class)]
+#[CoversClass(Dependencies::class)]
+#[CoversClass(Extension::class)]
+#[CoversClass(Extensions::class)]
+#[CoversClass(Factories::class)]
+#[CoversClass(Factory::class)]
+#[CoversClass(Inject::class)]
+#[CoversClass(Instances::class)]
+#[CoversClass(Providers::class)]
+#[CoversClass(Tags::class)]
 final class ServiceTagMustBeNonEmptyStringExceptionTest extends AbstractTestCase
 {
     /**
      * @throws Throwable
+     *
+     * @psalm-suppress InvalidArgument
      */
     public function testContainerTag(): void
     {
         $this->assertException(ServiceTagMustBeNonEmptyStringException::class);
 
-        $this->container->tag('service', ['']);
+        $this->container->tag(self::class, ['']);
     }
 
     /**
@@ -39,11 +59,13 @@ final class ServiceTagMustBeNonEmptyStringExceptionTest extends AbstractTestCase
     {
         $this->assertException(ServiceTagMustBeNonEmptyStringException::class);
 
-        $this->container->tag('service', [' ']);
+        $this->container->tag(self::class, [' ']);
     }
 
     /**
      * @throws Throwable
+     *
+     * @psalm-suppress InvalidArgument
      */
     public function testContainerTagged(): void
     {
