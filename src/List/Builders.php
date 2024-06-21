4c6f526d5f9f2cfdb9822e7ad8a9168aa6e98018
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:28 -0500

    Add Builders.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/List/Builders.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\List;
+
+use Closure;
+use Ghostwriter\Container\Exception\BuilderNotFoundException;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\ListInterface;
+
+use function array_key_exists;
+
+/**
+ * @template-covariant TService of object
+ */
+final class Builders implements ListInterface
+{
+    /**
+     * @param array<class-string<TService>,Closure(ContainerInterface):TService> $list
+     */
+    public function __construct(
+        private array $list = []
+    ) {
+    }
+
+    /**
+     * @param class-string<TService> $service
+     *
+     * @throws BuilderNotFoundException
+     *
+     * @return Closure(ContainerInterface):TService
+     *
+     */
+    public function get(string $service): Closure
+    {
+        return $this->list[$service] ?? throw new BuilderNotFoundException($service);
+    }
+
+    /**
+     * @param class-string<TService> $service
+     *
+     * @psalm-assert-if-true class-string<TService> $this->list[$service]
+     */
+    public function has(string $service): bool
+    {
+        return array_key_exists($service, $this->list);
+    }
+
+    /**
+     * @template TSet of object
+     *
+     * @param class-string<TSet>               $service
+     * @param Closure(ContainerInterface):TSet $value
+     */
+    public function set(string $service, Closure $value): void
+    {
+        /** @var self<TService|TSet> $this */
+        $this->list[$service] = $value;
+    }
+
+    /**
+     * @param class-string<TService> $service
+     */
+    public function unset(string $service): void
+    {
+        unset($this->list[$service]);
+    }
+
+    /**
+     * @template TNewService of object
+     *
+     * @param array<class-string<TNewService>,Closure(ContainerInterface):TNewService> $list
+     *
+     * @return self<TNewService>
+     */
+    public static function new(array $list = []): self
+    {
+        return new self($list);
+    }
+}
