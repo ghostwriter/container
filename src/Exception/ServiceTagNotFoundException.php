0bc1eaf249b4190f9b5affa12a5ab60393061ff5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:34 -0500

    Update ServiceTagNotFoundException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ServiceTagNotFoundException.php
+++ src/Exception/ServiceTagNotFoundException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Container\Exception;
 use Ghostwriter\Container\Interface\Exception\NotFoundExceptionInterface;
 use InvalidArgumentException;
 
-final class ServiceTagNotFoundException extends InvalidArgumentException implements NotFoundExceptionInterface {}
+final class ServiceTagNotFoundException extends InvalidArgumentException implements NotFoundExceptionInterface
+{
+}
