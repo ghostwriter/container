69b3a1dec71159bf8abc8399d876b6b82b287d61
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:33 -0500

    Update TypelessConstructor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/Constructor/TypelessConstructor.php
+++ tests/Fixture/Constructor/TypelessConstructor.php
@@ -2,20 +2,17 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Fixture\Constructor;
+namespace Tests\Fixture\Constructor;
 
-class TypelessConstructor
+final class TypelessConstructor
 {
-    /** @var mixed */
     public $result;
 
-    /** @param mixed $value */
     public function __construct($value)
     {
         $this->result = $value;
     }
 
-    /** @return mixed */
     public function value()
     {
         return $this->result;
