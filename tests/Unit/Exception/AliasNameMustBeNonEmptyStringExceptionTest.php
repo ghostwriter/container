3b3e058800528eaf3907f82de41f105a727c1f16
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-20 20:03:30 -0500

    Update AliasNameMustBeNonEmptyStringExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/AliasNameMustBeNonEmptyStringExceptionTest.php
+++ tests/Unit/Exception/AliasNameMustBeNonEmptyStringExceptionTest.php
@@ -2,23 +2,45 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\ContainerTests\Unit\Exception;
+namespace Tests\Unit\Exception;
 
+use Ghostwriter\Container\Attribute\Extension;
+use Ghostwriter\Container\Attribute\Factory;
+use Ghostwriter\Container\Attribute\Inject;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Container\Exception\AliasNameMustBeNonEmptyStringException;
-use Ghostwriter\Container\Instantiator;
-use Ghostwriter\Container\ParameterBuilder;
-use Ghostwriter\Container\Reflector;
-use Ghostwriter\ContainerTests\Unit\AbstractTestCase;
+use Ghostwriter\Container\List\Aliases;
+use Ghostwriter\Container\List\Bindings;
+use Ghostwriter\Container\List\Builders;
+use Ghostwriter\Container\List\Dependencies;
+use Ghostwriter\Container\List\Extensions;
+use Ghostwriter\Container\List\Factories;
+use Ghostwriter\Container\List\Instances;
+use Ghostwriter\Container\List\Providers;
+use Ghostwriter\Container\List\Tags;
 use PHPUnit\Framework\Attributes\CoversClass;
 use stdClass;
+use Tests\Unit\AbstractTestCase;
 use Throwable;
 
+/**
+ * @psalm-suppress ArgumentTypeCoercion
+ * @psalm-suppress UndefinedClass
+ */
 #[CoversClass(AliasNameMustBeNonEmptyStringException::class)]
-#[CoversClass(Instantiator::class)]
+#[CoversClass(Aliases::class)]
+#[CoversClass(Bindings::class)]
+#[CoversClass(Builders::class)]
 #[CoversClass(Container::class)]
-#[CoversClass(ParameterBuilder::class)]
-#[CoversClass(Reflector::class)]
+#[CoversClass(Dependencies::class)]
+#[CoversClass(Extension::class)]
+#[CoversClass(Extensions::class)]
+#[CoversClass(Factories::class)]
+#[CoversClass(Factory::class)]
+#[CoversClass(Inject::class)]
+#[CoversClass(Instances::class)]
+#[CoversClass(Providers::class)]
+#[CoversClass(Tags::class)]
 final class AliasNameMustBeNonEmptyStringExceptionTest extends AbstractTestCase
 {
     /**
@@ -29,7 +51,8 @@ final class AliasNameMustBeNonEmptyStringExceptionTest extends AbstractTestCase
         $this->assertException(AliasNameMustBeNonEmptyStringException::class);
 
         $this->container
-            ->alias('', stdClass::class);
+            ->alias(stdClass::class, '')
+        ;
     }
 
     /**
@@ -40,6 +63,7 @@ final class AliasNameMustBeNonEmptyStringExceptionTest extends AbstractTestCase
         $this->assertException(AliasNameMustBeNonEmptyStringException::class);
 
         $this->container
-            ->alias(' ', stdClass::class);
+            ->alias(stdClass::class, ' ')
+        ;
     }
 }
