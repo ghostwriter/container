98506c3a35a9398243c73327f6564f44fb60f93f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-09 13:15:29 -0500

    Update Providers.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/List/Providers.php
+++ src/List/Providers.php
@@ -7,8 +7,6 @@ namespace Ghostwriter\Container\List;
 use Ghostwriter\Container\Interface\ListInterface;
 use Ghostwriter\Container\Interface\ServiceProviderInterface;
 
-use function array_key_exists;
-
 final class Providers implements ListInterface
 {
     /**
@@ -17,6 +15,14 @@ final class Providers implements ListInterface
     public function __construct(
         private array $list = []
     ) {
+    }
+
+    /**
+     * @param array<class-string<ServiceProviderInterface>,bool> $list
+     */
+    public static function new(array $list = []): self
+    {
+        return new self($list);
     }
 
     /**
@@ -32,19 +38,11 @@ final class Providers implements ListInterface
      */
     public function has(string $serviceProvider): bool
     {
-        return array_key_exists($serviceProvider, $this->list);
+        return \array_key_exists($serviceProvider, $this->list);
     }
 
     public function unset(string $serviceProvider): void
     {
         unset($this->list[$serviceProvider]);
-    }
-
-    /**
-     * @param array<class-string<ServiceProviderInterface>,bool> $list
-     */
-    public static function new(array $list = []): self
-    {
-        return new self($list);
     }
 }
