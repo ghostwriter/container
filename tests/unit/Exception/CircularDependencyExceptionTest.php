4c21b9be630403fb8a5b1fefd8f5e2ba1e0670b9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-01 15:17:52 -0600

    Update CircularDependencyExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Exception/CircularDependencyExceptionTest.php
+++ tests/unit/Exception/CircularDependencyExceptionTest.php
@@ -28,6 +28,9 @@ use Tests\Fixture\CircularDependency\ClassZ;
 use Tests\Unit\AbstractTestCase;
 use Throwable;
 
+use function implode;
+use function sprintf;
+
 #[CoversClass(CircularDependencyException::class)]
 #[CoversClass(Aliases::class)]
 #[CoversClass(Bindings::class)]
@@ -50,9 +53,9 @@ final class CircularDependencyExceptionTest extends AbstractTestCase
     public function testContainerBuild(): void
     {
         $this->assertException(CircularDependencyException::class);
-        $this->expectExceptionMessage(\sprintf(
+        $this->expectExceptionMessage(sprintf(
             'Class: %s',
-            \implode(
+            implode(
                 ' -> ',
                 [
                     ClassA::class,
