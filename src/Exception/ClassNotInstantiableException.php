c77867a51de76fc96d2372b9da8a835186e082cb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-01 15:17:48 -0600

    Update ClassNotInstantiableException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ClassNotInstantiableException.php
+++ src/Exception/ClassNotInstantiableException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\Container\Exception;
 use Ghostwriter\Container\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class ClassNotInstantiableException extends InvalidArgumentException implements ExceptionInterface
-{
-}
+final class ClassNotInstantiableException extends InvalidArgumentException implements ExceptionInterface {}
