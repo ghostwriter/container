8482d747b638c07749a23d6bc9b31207e7941484
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:37 -0600

    Update BuildersTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/List/BuildersTest.php
+++ tests/unit/List/BuildersTest.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace Tests\Unit\List;
 
+use Ghostwriter\Container\Exception\BuilderAlreadyExistsException;
 use Ghostwriter\Container\List\Builders;
+use Ghostwriter\Container\Name\Service;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
+#[CoversClass(Service::class)]
 #[CoversClass(Builders::class)]
 final class BuildersTest extends TestCase
 {
-    public function testExample(): void
+    public function testThrowsBuilderAlreadyExistsException(): void
     {
-        self::assertTrue(true);
+        $this->expectException(BuilderAlreadyExistsException::class);
+
+        $builders = Builders::new();
+
+        $builders->set(stdClass::class, static fn (): stdClass => new stdClass());
+
+        $builders->set(stdClass::class, static fn (): stdClass => new stdClass());
     }
 }
