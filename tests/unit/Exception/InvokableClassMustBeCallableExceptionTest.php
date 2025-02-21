6744c9e6e773c8d9abeec5214450239f1afdca39
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:36 -0600

    Update InvokableClassMustBeCallableExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Exception/InvokableClassMustBeCallableExceptionTest.php
+++ tests/unit/Exception/InvokableClassMustBeCallableExceptionTest.php
@@ -4,15 +4,41 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Exception;
 
+use Ghostwriter\Container\Container;
 use Ghostwriter\Container\Exception\InvokableClassMustBeCallableException;
+use Ghostwriter\Container\List\Aliases;
+use Ghostwriter\Container\List\Bindings;
+use Ghostwriter\Container\List\Builders;
+use Ghostwriter\Container\List\Dependencies;
+use Ghostwriter\Container\List\Extensions;
+use Ghostwriter\Container\List\Factories;
+use Ghostwriter\Container\List\Instances;
+use Ghostwriter\Container\List\Providers;
+use Ghostwriter\Container\List\Tags;
+use Ghostwriter\Container\Name\Service;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
+#[CoversClass(Container::class)]
+#[CoversClass(Aliases::class)]
+#[CoversClass(Builders::class)]
+#[CoversClass(Dependencies::class)]
+#[CoversClass(Extensions::class)]
+#[CoversClass(Factories::class)]
+#[CoversClass(Instances::class)]
+#[CoversClass(Service::class)]
+#[CoversClass(Bindings::class)]
+#[CoversClass(Providers::class)]
+#[CoversClass(Tags::class)]
 #[CoversClass(InvokableClassMustBeCallableException::class)]
 final class InvokableClassMustBeCallableExceptionTest extends TestCase
 {
-    public function testExample(): void
+    public function testThrowsInvokableClassMustBeCallableException(): void
     {
-        self::assertTrue(true);
+        $this->expectException(InvokableClassMustBeCallableException::class);
+
+        $container = Container::getInstance();
+
+        $container->invoke(InvokableClassMustBeCallableException::class);
     }
 }
