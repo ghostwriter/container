3627439ccd925a889d527f6574b579c4a7ba0964
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:35 -0600

    Update AliasNameMustBeNonEmptyStringExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Exception/AliasNameMustBeNonEmptyStringExceptionTest.php
+++ tests/unit/Exception/AliasNameMustBeNonEmptyStringExceptionTest.php
@@ -18,6 +18,7 @@ use Ghostwriter\Container\List\Factories;
 use Ghostwriter\Container\List\Instances;
 use Ghostwriter\Container\List\Providers;
 use Ghostwriter\Container\List\Tags;
+use Ghostwriter\Container\Name\Alias;
 use PHPUnit\Framework\Attributes\CoversClass;
 use stdClass;
 use Tests\Unit\AbstractTestCase;
@@ -41,6 +42,7 @@ use Throwable;
 #[CoversClass(Instances::class)]
 #[CoversClass(Providers::class)]
 #[CoversClass(Tags::class)]
+#[CoversClass(Alias::class)]
 final class AliasNameMustBeNonEmptyStringExceptionTest extends AbstractTestCase
 {
     /**
