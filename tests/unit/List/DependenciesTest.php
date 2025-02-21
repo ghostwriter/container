0d3ef5c073055e7a8e47ca01d04284dfe0591599
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:37 -0600

    Update DependenciesTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/List/DependenciesTest.php
+++ tests/unit/List/DependenciesTest.php
@@ -4,15 +4,43 @@ declare(strict_types=1);
 
 namespace Tests\Unit\List;
 
+use Ghostwriter\Container\Exception\ShouldNotHappenException;
 use Ghostwriter\Container\List\Dependencies;
+use Ghostwriter\Container\Name\Service;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
+#[CoversClass(Service::class)]
 #[CoversClass(Dependencies::class)]
 final class DependenciesTest extends TestCase
 {
-    public function testExample(): void
+    public function testLast(): void
     {
-        self::assertTrue(true);
+        $dependencies = Dependencies::new();
+
+        $dependencies->set(stdClass::class);
+
+        self::assertSame(stdClass::class, $dependencies->last());
+    }
+
+    public function testLastThrows(): void
+    {
+        $dependencies = Dependencies::new();
+
+        $this->expectException(ShouldNotHappenException::class);
+
+        $dependencies->last();
+    }
+
+    public function testMissing(): void
+    {
+        $dependencies = Dependencies::new();
+
+        self::assertFalse($dependencies->missing());
+
+        $dependencies->set(stdClass::class);
+
+        self::assertTrue($dependencies->missing());
     }
 }
