11248f83545809b81de2b571384728211d071af9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:34 -0600

    Update InjectTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Attribute/InjectTest.php
+++ tests/unit/Attribute/InjectTest.php
@@ -17,8 +17,14 @@ use Ghostwriter\Container\List\Factories;
 use Ghostwriter\Container\List\Instances;
 use Ghostwriter\Container\List\Providers;
 use Ghostwriter\Container\List\Tags;
+use Ghostwriter\Container\Name\Alias;
+use Ghostwriter\Container\Name\Service;
 use PHPUnit\Framework\Attributes\CoversClass;
-use Tests\Fixture\Attribute\ClassParameterHasInjectAttribute;
+use Tests\Fixture\Attribute\Factory\Foobar2;
+use Tests\Fixture\Attribute\Foobar2Interface;
+use Tests\Fixture\Attribute\Inject\ClassHasInjectAttribute;
+use Tests\Fixture\Attribute\Inject\ClassParameterHasInjectAttribute;
+use Tests\Fixture\Attribute\Inject\InvokableClassHasInjectAttribute;
 use Tests\Fixture\GitHubClient;
 use Tests\Unit\AbstractTestCase;
 
@@ -35,13 +41,33 @@ use Tests\Unit\AbstractTestCase;
 #[CoversClass(Instances::class)]
 #[CoversClass(Providers::class)]
 #[CoversClass(Tags::class)]
+#[CoversClass(Service::class)]
+#[CoversClass(Alias::class)]
+#[CoversClass(\Ghostwriter\Container\Name\Factory::class)]
 final class InjectTest extends AbstractTestCase
 {
+    public function testClassHasInjectAttribute(): void
+    {
+        self::assertInstanceOf(
+            Foobar2Interface::class,
+            $this->container->get(ClassHasInjectAttribute::class)->foobar(),
+        );
+
+        $classHasInjectAttribute = $this->container->get(ClassHasInjectAttribute::class);
+        self::assertInstanceOf(ClassHasInjectAttribute::class, $classHasInjectAttribute);
+        self::assertInstanceOf(Foobar2::class, $classHasInjectAttribute->foobar());
+    }
+
     public function testClassParameterHasInjectAttribute(): void
     {
         self::assertInstanceOf(
             GitHubClient::class,
             $this->container->get(ClassParameterHasInjectAttribute::class)->getClient()
         );
+    }
+
+    public function testInvokableClassHasInjectAttribute(): void
+    {
+        self::assertInstanceOf(Foobar2::class, $this->container->invoke(InvokableClassHasInjectAttribute::class));
     }
 }
