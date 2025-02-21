d7e5d6b9dad55b28eb38224289d848dbc04f4921
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:34 -0600

    Update FactoryTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Attribute/FactoryTest.php
+++ tests/unit/Attribute/FactoryTest.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Attribute;
 
+use Generator;
 use Ghostwriter\Container\Attribute\Extension;
 use Ghostwriter\Container\Attribute\Factory;
-use Ghostwriter\Container\Attribute\Inject;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Container\List\Aliases;
 use Ghostwriter\Container\List\Bindings;
@@ -17,9 +17,15 @@ use Ghostwriter\Container\List\Factories;
 use Ghostwriter\Container\List\Instances;
 use Ghostwriter\Container\List\Providers;
 use Ghostwriter\Container\List\Tags;
+use Ghostwriter\Container\Name\Alias;
+use Ghostwriter\Container\Name\Factory as FactoryName;
+use Ghostwriter\Container\Name\Service as ServiceName;
 use PHPUnit\Framework\Attributes\CoversClass;
-use Tests\Fixture\Attribute\ClassHasFactoryAttribute;
-use Tests\Fixture\Attribute\ClassParameterHasFactoryAttribute;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Tests\Fixture\Attribute\Factory\ClassHasFactoryAttribute;
+use Tests\Fixture\Attribute\Factory\ClassParameterHasClassWithFactoryAttribute;
+use Tests\Fixture\Dummy;
+use Tests\Fixture\Factory\DummyFactory;
 use Tests\Fixture\Foobar;
 use Tests\Unit\AbstractTestCase;
 
@@ -32,22 +38,37 @@ use Tests\Unit\AbstractTestCase;
 #[CoversClass(Extensions::class)]
 #[CoversClass(Factories::class)]
 #[CoversClass(Factory::class)]
-#[CoversClass(Inject::class)]
 #[CoversClass(Instances::class)]
 #[CoversClass(Providers::class)]
 #[CoversClass(Tags::class)]
+#[CoversClass(ServiceName::class)]
+#[CoversClass(FactoryName::class)]
+#[CoversClass(Alias::class)]
 final class FactoryTest extends AbstractTestCase
 {
-    public function testClassHasFactoryAttribute(): void
+    #[DataProvider('provide')]
+    public function testClassHasFactoryAttribute(string $class): void
     {
-        self::assertInstanceOf(Foobar::class, $this->container->get(ClassHasFactoryAttribute::class)->foobar());
+        self::assertInstanceOf($class, $this->container->get($class));
+    }
+
+    public function testClassHasFactoryAttribute2(): void
+    {
+        self::assertInstanceOf(DummyFactory::class, $this->container->get(Dummy::class)->getDummyFactory());
     }
 
     public function testClassParameterHasFactoryAttribute(): void
     {
         self::assertInstanceOf(
             Foobar::class,
-            $this->container->get(ClassParameterHasFactoryAttribute::class)->foobar()
+            $this->container->get(ClassParameterHasClassWithFactoryAttribute::class)->foobar()
         );
+    }
+
+    public static function provide(): Generator
+    {
+        foreach ([Dummy::class, ClassHasFactoryAttribute::class] as $class) {
+            yield $class => [$class];
+        }
     }
 }
