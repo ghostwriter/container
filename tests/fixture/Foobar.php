e4249dbeecd9990a93aa1485b0abd4642f40182d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:34 -0600

    Update Foobar.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Foobar.php
+++ tests/fixture/Foobar.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Tests\Fixture;
 
+use Ghostwriter\Container\Attribute\Factory;
+use Tests\Fixture\Factory\FoobarFactory;
+
+#[Factory(FoobarFactory::class)]
 final class Foobar
 {
     public function __construct(
