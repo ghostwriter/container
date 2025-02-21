4b50ff266d71297f7a39ba884d2f6693ce852484
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:37 -0600

    Update AliasesTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/List/AliasesTest.php
+++ tests/unit/List/AliasesTest.php
@@ -4,15 +4,32 @@ declare(strict_types=1);
 
 namespace Tests\Unit\List;
 
+use Error;
+use Ghostwriter\Container\Container;
+use Ghostwriter\Container\Exception\AliasNotFoundException;
+use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\List\Aliases;
+use Ghostwriter\Container\Name\Alias;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
+#[CoversClass(Alias::class)]
 #[CoversClass(Aliases::class)]
 final class AliasesTest extends TestCase
 {
-    public function testExample(): void
+    public function testGet(): void
     {
-        self::assertTrue(true);
+        $aliases = Aliases::new();
+
+        self::assertSame(Container::class, $aliases->get(ContainerInterface::class));
+    }
+
+    public function testGetError(): void
+    {
+        $aliases = Aliases::new();
+
+        $this->expectException(AliasNotFoundException::class);
+
+        $aliases->get(Error::class);
     }
 }
