04b6af9435019acfa3da5b1798f00a73e414101b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:37 -0600

    Update BindingsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/List/BindingsTest.php
+++ tests/unit/List/BindingsTest.php
@@ -4,15 +4,27 @@ declare(strict_types=1);
 
 namespace Tests\Unit\List;
 
+use Ghostwriter\Container\Container;
+use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\List\Bindings;
+use Ghostwriter\Container\Name\Alias;
+use Ghostwriter\Container\Name\Service;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
+#[CoversClass(Service::class)]
 #[CoversClass(Bindings::class)]
 final class BindingsTest extends TestCase
 {
-    public function testExample(): void
+    public function testContains(): void
     {
-        self::assertTrue(true);
+        $bindings = Bindings::new();
+
+        self::assertFalse($bindings->contains(ContainerInterface::class));
+
+        $bindings->set(Alias::class, Alias::class, Container::class);
+        $bindings->set(Service::class, ContainerInterface::class, Container::class);
+
+        self::assertTrue($bindings->contains(ContainerInterface::class));
     }
 }
