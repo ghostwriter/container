5c0f31dedf76c1c682be31da7ea0e93282e61d12
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:22 -0600

    Add BuilderAlreadyExistsException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/BuilderAlreadyExistsException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Container\Exception;
+
+use Ghostwriter\Container\Interface\ExceptionInterface;
+use InvalidArgumentException;
+
+final class BuilderAlreadyExistsException extends InvalidArgumentException implements ExceptionInterface {}
