87d76469963efe4136f449cba00f88fd6ac13a26
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-20 23:11:32 -0600

    Move ClassHasInjectAttribute.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Attribute/Inject/ClassHasInjectAttribute.php
+++ tests/fixture/Attribute/Inject/ClassHasInjectAttribute.php
@@ -1,19 +1,17 @@
 <?php
 
-namespace Tests\Fixture\Attribute;
+namespace Tests\Fixture\Attribute\Inject;
 
-use Ghostwriter\Container\Attribute\Factory;
 use Ghostwriter\Container\Attribute\Inject;
+use Tests\Fixture\Attribute\Factory\Foobar2;
+use Tests\Fixture\Attribute\Foobar2Interface;
 
-final readonly class ClassHasFactoryAttribute2
+final readonly class ClassHasInjectAttribute
 {
     public function __construct(
         #[Inject(Foobar2::class)]
-        #[Factory(Foobar2Factory::class)]
         private Foobar2Interface $foobar
-    )
-    {
-    }
+    ) {}
 
     public function foobar(): Foobar2Interface
     {
