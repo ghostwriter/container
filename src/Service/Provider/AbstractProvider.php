c5b7e5d4567ca53f6e57bb085a2c8a6f91a80659
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-04-22 12:13:30 -0500

    Add ALIAS/EXTEND/FACTORY constants to AbstractProvider
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/Provider/AbstractProvider.php
+++ src/Service/Provider/AbstractProvider.php
@@ -6,12 +6,29 @@ namespace Ghostwriter\Container\Service\Provider;
 
 use Ghostwriter\Container\Interface\BuilderInterface;
 use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\Service\ExtensionInterface;
+use Ghostwriter\Container\Interface\Service\FactoryInterface;
 use Ghostwriter\Container\Interface\Service\ProviderInterface;
 use Override;
 use Throwable;
 
 abstract class AbstractProvider implements ProviderInterface
 {
+    /** @var array<class-string,class-string> */
+    protected const array ALIAS = [
+        // alias => service
+    ];
+
+    /** @var array<class-string,list<class-string<ExtensionInterface>>> */
+    protected const array EXTEND = [
+        // service => [extension, ...]
+    ];
+
+    /** @var array<class-string,class-string<FactoryInterface>> */
+    protected const array FACTORY = [
+        // service => factory
+    ];
+
     /** @throws Throwable */
     #[Override]
     public function boot(ContainerInterface $container): void
@@ -23,6 +40,18 @@ abstract class AbstractProvider implements ProviderInterface
     #[Override]
     public function register(BuilderInterface $builder): void
     {
-        // no-op
+        foreach (static::ALIAS as $alias => $service) {
+            $builder->alias($alias, $service);
+        }
+
+        foreach (static::EXTEND as $service => $extensions) {
+            foreach ($extensions as $extension) {
+                $builder->extend($service, $extension);
+            }
+        }
+
+        foreach (static::FACTORY as $service => $factory) {
+            $builder->factory($service, $factory);
+        }
     }
 }
